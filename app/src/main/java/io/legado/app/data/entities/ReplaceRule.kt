15c3eb8dd27f94496032fe27dd1dbceca489a273
atbest <atbest09@gmail.com>
2019-05-22 22:42:50 -0400

    Added database entities

---
+++ app/src/main/java/io/legado/app/data/entities/ReplaceRule.kt
@@ -0,0 +1,23 @@
+package io.legado.app.data.entities
+
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.Index
+import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+@Entity(tableName = "replace_rules",
+    indices = [(Index(value = ["id"]))])
+data class ReplaceRule(@PrimaryKey
+                       val id: Int = 0,
+                       val summary: String? = null,
+                       val pattern: String? = null,
+                       val replacement: String? = null,
+                       val scope: String? = null,
+                       val isEnabled: Boolean? = null,
+                       val isRegex: Boolean? = null,
+                       val order: Int = 0
+) : Parcelable
+
+
