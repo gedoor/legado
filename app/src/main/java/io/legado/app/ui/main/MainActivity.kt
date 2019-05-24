1c82acdf982459f29b1c55e494f66b3cf62744cc
Invinciblelee <1760316362@qq.com>
2019-05-24 09:52:49 +0800

    ViewModelExtensions

--- app/src/main/java/io/legado/app/ui/main/MainActivity.kt
+++ app/src/main/java/io/legado/app/ui/main/MainActivity.kt
@@ -58,21 +58,6 @@ class MainActivity : BaseActivity<MainDataBinding, MainViewModel>(), NavigationV
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         // Handle navigation view item clicks here.
         when (item.itemId) {
-            R.id.nav_home -> {
-                // Handle the camera action
-            }
-            R.id.nav_gallery -> {
-
-            }
-            R.id.nav_slideshow -> {
-
-            }
-            R.id.nav_tools -> {
-
-            }
-            R.id.nav_share -> {
-
-            }
             R.id.nav_send -> {
 
             }
