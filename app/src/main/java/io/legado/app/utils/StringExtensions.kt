36eb40c813ed6700db5a7a7f65bc0d2ae55a6b65
atbest <atbest09@gmail.com>
2019-05-25 06:24:20 -0400

    Remove slf4j from jsonpath

--- app/src/main/java/io/legado/app/utils/StringExtensions.kt
+++ app/src/main/java/io/legado/app/utils/StringExtensions.kt
@@ -1,4 +1,5 @@
 package io.legado.app.utils
+// import org.apache.commons.text.StringEscapeUtils
 
 fun String?.strim() = if (this.isNullOrBlank()) null else this.trim()
 
@@ -8,3 +9,4 @@ fun String.isAbsUrl() = this.startsWith("http://", true)
 fun String.splitNotBlank(delim: String) = if (!this.contains(delim)) sequenceOf(this) else
         this.split(delim).asSequence().map { it.trim() }.filterNot { it.isBlank() }
 
+// fun String.unescapeJson() = StringEscapeUtils.unescapeJson(this)
