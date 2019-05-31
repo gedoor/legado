e29e6fcbaa323af6ef1e024305a455c75ac3a54b
GKF <gekunfei@live.com>
2019-05-31 09:16:07 +0800

    up aboutActivity

--- app/src/main/java/io/legado/app/ui/about/AboutActivity.kt
+++ app/src/main/java/io/legado/app/ui/about/AboutActivity.kt
@@ -1,13 +1,19 @@
 package io.legado.app.ui.about
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.AndroidViewModel
 import io.legado.app.R
+import io.legado.app.base.BaseActivity
+import io.legado.app.utils.getViewModel
 
-class AboutActivity : AppCompatActivity() {
+class AboutActivity : BaseActivity<AndroidViewModel>() {
+    override val viewModel: AndroidViewModel
+        get() = getViewModel(AndroidViewModel::class.java)
+    override val layoutID: Int
+        get() = R.layout.activity_about
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_about)
+    override fun onViewModelCreated(viewModel: AndroidViewModel, savedInstanceState: Bundle?) {
+        super.onViewModelCreated(viewModel, savedInstanceState)
     }
+
 }
