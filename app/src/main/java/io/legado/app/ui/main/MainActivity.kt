973a00007f23f01355d91ce4b2a76309d547dea9
gedoor <gekunfei@live.com>
2019-06-02 20:29:04 +0800

    优化

--- app/src/main/java/io/legado/app/ui/main/MainActivity.kt
+++ app/src/main/java/io/legado/app/ui/main/MainActivity.kt
@@ -4,24 +4,16 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.core.view.GravityCompat
-import androidx.drawerlayout.widget.DrawerLayout
-import com.google.android.material.navigation.NavigationView
 import io.legado.app.R
 import io.legado.app.base.BaseActivity
 import io.legado.app.help.permission.Permissions
 import io.legado.app.help.permission.PermissionsCompat
-import io.legado.app.help.storage.Backup
 import io.legado.app.help.storage.Restore
-import io.legado.app.ui.replacerule.ReplaceRuleActivity
 import io.legado.app.ui.search.SearchActivity
 import io.legado.app.utils.getViewModel
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.app_bar_main.*
-import org.jetbrains.anko.startActivity
 
-class MainActivity : BaseActivity<MainViewModel>(), NavigationView.OnNavigationItemSelectedListener {
+class MainActivity : BaseActivity<MainViewModel>() {
     override val viewModel: MainViewModel
         get() = getViewModel(MainViewModel::class.java)
 
@@ -31,24 +23,7 @@ class MainActivity : BaseActivity<MainViewModel>(), NavigationView.OnNavigationI
     override fun onViewModelCreated(viewModel: MainViewModel, savedInstanceState: Bundle?) {
         fab.setOnClickListener { startActivity(Intent(this, SearchActivity::class.java)) }
 
-        val toggle = ActionBarDrawerToggle(
-            this, drawer_layout, title_bar.toolbar,
-            R.string.navigation_drawer_open,
-            R.string.navigation_drawer_close
-        )
-        drawer_layout.addDrawerListener(toggle)
-        toggle.syncState()
 
-        nav_view.setNavigationItemSelectedListener(this)
-    }
-
-    override fun onBackPressed() {
-        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
-        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
-            drawerLayout.closeDrawer(GravityCompat.START)
-        } else {
-            super.onBackPressed()
-        }
     }
 
     override fun onCompatCreateOptionsMenu(menu: Menu): Boolean {
@@ -58,23 +33,6 @@ class MainActivity : BaseActivity<MainViewModel>(), NavigationView.OnNavigationI
 
     override fun onCompatOptionsItemSelected(item: MenuItem): Boolean {
         return super.onCompatOptionsItemSelected(item)
-    }
-
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        // Handle navigation view item clicks here.
-        when (item.itemId) {
-            R.id.nav_backup -> Backup.backup()
-            R.id.nav_restore -> Restore.restore()
-            R.id.nav_import_old -> importYueDu()
-            R.id.nav_import_github -> Restore.importFromGithub()
-            R.id.nav_replace_rule -> startActivity<ReplaceRuleActivity>()
-            R.id.nav_send -> {
-
-            }
-        }
-        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
-        drawerLayout.closeDrawer(GravityCompat.START)
-        return true
     }
 
     private fun importYueDu() {
