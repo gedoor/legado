6e804e7b11b94664732f457fe247509e99a95b83
gedoor <gekunfei@live.com>
2019-06-04 21:02:29 +0800

    优化

--- app/src/main/java/io/legado/app/ui/main/booksource/BookSourceFragment.kt
+++ app/src/main/java/io/legado/app/ui/main/booksource/BookSourceFragment.kt
@@ -2,15 +2,24 @@ package io.legado.app.ui.main.booksource
 
 import android.os.Bundle
 import android.util.Log
+import android.view.MenuItem
 import android.view.View
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import io.legado.app.R
+import kotlinx.android.synthetic.main.view_titlebar.*
 
-class BookSourceFragment : Fragment(R.layout.fragment_book_source) {
+class BookSourceFragment : Fragment(R.layout.fragment_book_source), Toolbar.OnMenuItemClickListener {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
          Log.e("TAG", "BookSourceFragment")
+        toolbar.inflateMenu(R.menu.book_source)
+        toolbar.setOnMenuItemClickListener(this)
+    }
+
+    override fun onMenuItemClick(item: MenuItem?): Boolean {
+        return false
     }
 
 }
--- app/src/main/java/io/legado/app/ui/main/myconfig/MyConfigFragment.kt
+++ app/src/main/java/io/legado/app/ui/main/myconfig/MyConfigFragment.kt
@@ -2,15 +2,24 @@ package io.legado.app.ui.main.myconfig
 
 import android.os.Bundle
 import android.util.Log
+import android.view.MenuItem
 import android.view.View
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import io.legado.app.R
+import kotlinx.android.synthetic.main.view_titlebar.*
 
-class MyConfigFragment : Fragment(R.layout.fragment_my_config) {
+class MyConfigFragment : Fragment(R.layout.fragment_my_config), Toolbar.OnMenuItemClickListener {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         Log.e("TAG", "MyConfigFragment")
+        toolbar.inflateMenu(R.menu.my_config)
+        toolbar.setOnMenuItemClickListener(this)
+    }
+
+    override fun onMenuItemClick(item: MenuItem?): Boolean {
+        return false
     }
 
 }
