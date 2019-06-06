510bf822837e4b5d94f6b1b4bf33a791cafd41d9
GKF <gekunfei@live.com>
2019-06-06 16:39:56 +0800

    up

--- app/src/main/java/io/legado/app/ui/main/booksource/BookSourceAdapter.kt
+++ app/src/main/java/io/legado/app/ui/main/booksource/BookSourceAdapter.kt
@@ -1,2 +1,40 @@
 package io.legado.app.ui.main.booksource
 
+import android.view.View
+import android.view.ViewGroup
+import androidx.paging.PagedListAdapter
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import io.legado.app.data.entities.Source
+
+class BookSourceAdapter : PagedListAdapter<Source, BookSourceAdapter.MyViewHolder>(DIFF_CALLBACK) {
+
+    companion object {
+
+        @JvmField
+        val DIFF_CALLBACK = object : DiffUtil.ItemCallback<Source>() {
+            override fun areItemsTheSame(oldItem: Source, newItem: Source): Boolean =
+                oldItem.id == newItem.id
+
+            override fun areContentsTheSame(oldItem: Source, newItem: Source): Boolean =
+                oldItem.id == newItem.id
+                        && oldItem.name == newItem.name
+                        && oldItem.group == newItem.group
+                        && oldItem.isEnabled == newItem.isEnabled
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+
+    }
+
+
+    class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    }
+}
