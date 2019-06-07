0044cad2f67fbd67b2ab93725bd14836909099e3
gedoor <gekunfei@live.com>
2019-06-07 18:33:07 +0800

    优化

--- app/src/main/java/io/legado/app/ui/sourceedit/SourceEditAdapter.kt
+++ app/src/main/java/io/legado/app/ui/sourceedit/SourceEditAdapter.kt
@@ -1,13 +1,17 @@
 package io.legado.app.ui.sourceedit
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import io.legado.app.R
 
 class SourceEditAdapter : RecyclerView.Adapter<SourceEditAdapter.MyViewHolder>() {
 
+    var sourceEditEntities:ArrayList<SourceEditEntity> = ArrayList()
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return MyViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_source_edit, parent, false))
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
@@ -15,10 +19,16 @@ class SourceEditAdapter : RecyclerView.Adapter<SourceEditAdapter.MyViewHolder>()
     }
 
     override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return sourceEditEntities.size
     }
 
     class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
+    }
+
+    class SourceEditEntity {
+        var key:String?=null
+        var value:String?=null
+        var hint:String?=null
     }
 }
