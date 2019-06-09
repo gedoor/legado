68f4c7a87a8d19dff27ef2f49afd421027cfff49
gedoor <gekunfei@live.com>
2019-06-09 20:35:47 +0800

    up

--- app/src/main/java/io/legado/app/ui/main/bookshelf/BookGroupAdapter.kt
+++ app/src/main/java/io/legado/app/ui/main/bookshelf/BookGroupAdapter.kt
@@ -1,10 +1,12 @@
 package io.legado.app.ui.main.bookshelf
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.paging.PagedListAdapter
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import io.legado.app.R
 import io.legado.app.data.entities.BookGroup
 
 class BookGroupAdapter : PagedListAdapter<BookGroup, BookGroupAdapter.MyViewHolder>(DIFF_CALLBACK) {
@@ -23,11 +25,11 @@ class BookGroupAdapter : PagedListAdapter<BookGroup, BookGroupAdapter.MyViewHold
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return MyViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_book_group, parent, false))
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
     }
 
     class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
