444c46f5cc648e0a20849c2e44708db6eff38550
GKF <gekunfei@live.com>
2019-06-10 08:44:55 +0800

    up

--- app/src/main/java/io/legado/app/data/dao/BookGroupDao.kt
+++ app/src/main/java/io/legado/app/data/dao/BookGroupDao.kt
@@ -1,9 +1,11 @@
 package io.legado.app.data.dao
 
 import androidx.paging.DataSource
+import androidx.room.Dao
 import androidx.room.Query
 import io.legado.app.data.entities.BookGroup
 
+@Dao
 interface BookGroupDao {
 
     @Query("SELECT * FROM book_groups ORDER BY `order`")
