59f74e05d8cc9e4e3933abbb19603ccf94c96a08
GKF <gekunfei@live.com>
2019-06-11 14:24:53 +0800

    up

--- app/src/main/java/io/legado/app/ui/main/myconfig/PreferenceFragment.kt
+++ app/src/main/java/io/legado/app/ui/main/myconfig/PreferenceFragment.kt
@@ -6,6 +6,7 @@ import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import io.legado.app.App
 import io.legado.app.R
+import io.legado.app.ui.about.AboutActivity
 import io.legado.app.ui.config.ConfigActivity
 import io.legado.app.ui.config.ConfigViewModel
 import org.jetbrains.anko.startActivity
@@ -39,19 +40,20 @@ class PreferenceFragment : PreferenceFragmentCompat(), SharedPreferences.OnShare
             when (preference.key) {
                 "setting" -> {
                     requireContext().startActivity<ConfigActivity>(
-                        Pair("configType", ConfigViewModel.TYPE_CONFIG)
+                            Pair("configType", ConfigViewModel.TYPE_CONFIG)
                     )
                 }
                 "web_dav_setting" -> {
                     requireContext().startActivity<ConfigActivity>(
-                        Pair("configType", ConfigViewModel.TYPE_WEB_DAV_CONFIG)
+                            Pair("configType", ConfigViewModel.TYPE_WEB_DAV_CONFIG)
                     )
                 }
                 "theme_setting" -> {
                     requireContext().startActivity<ConfigActivity>(
-                        Pair("configType", ConfigViewModel.TYPE_THEME_CONFIG)
+                            Pair("configType", ConfigViewModel.TYPE_THEME_CONFIG)
                     )
                 }
+                "about" -> requireContext().startActivity<AboutActivity>()
             }
         }
         return super.onPreferenceTreeClick(preference)
