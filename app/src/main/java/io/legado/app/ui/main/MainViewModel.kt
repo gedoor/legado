8fb9dbde388375445ec1a2e2f1d36f31e89a4539
gedoor <gekunfei@live.com>
2019-06-13 20:13:23 +0800

    up

--- app/src/main/java/io/legado/app/ui/main/MainViewModel.kt
+++ app/src/main/java/io/legado/app/ui/main/MainViewModel.kt
@@ -2,8 +2,15 @@ package io.legado.app.ui.main
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import io.legado.app.help.storage.Restore
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainViewModel(application: Application) : AndroidViewModel(application) {
 
-
+    fun restore() {
+        GlobalScope.launch {
+            Restore.importYueDuData(getApplication())
+        }
+    }
 }
