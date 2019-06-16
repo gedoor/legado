364ee607d7f80ff126b7941a8f1f1ea6cd0bb9a5
gedoor <gekunfei@live.com>
2019-06-16 20:16:35 +0800

    up

--- app/src/main/java/io/legado/app/ui/bookshelf/BookshelfActivity.kt
+++ app/src/main/java/io/legado/app/ui/bookshelf/BookshelfActivity.kt
@@ -1,6 +1,9 @@
 package io.legado.app.ui.bookshelf
 
 import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import io.legado.app.R
 import io.legado.app.base.BaseActivity
 import io.legado.app.utils.getViewModel
@@ -12,12 +15,31 @@ class BookshelfActivity : BaseActivity<BookshelfViewModel>() {
     override val layoutID: Int
         get() = R.layout.activity_bookshelf
 
+    private lateinit var bookshelfAdapter: BookshelfAdapter
+
     override fun onViewModelCreated(viewModel: BookshelfViewModel, savedInstanceState: Bundle?) {
         if (viewModel.bookGroup == null) {
             viewModel.bookGroup = intent.getParcelableExtra("data")
         }
         viewModel.bookGroup?.let {
             title_bar.title = it.groupName
+        }
+        initRecyclerView()
+        upRecyclerData()
+    }
+
+    private fun initRecyclerView() {
+        rv_bookshelf.layoutManager = LinearLayoutManager(this)
+        rv_bookshelf.addItemDecoration(DividerItemDecoration(this, LinearLayout.VERTICAL))
+        bookshelfAdapter = BookshelfAdapter()
+        rv_bookshelf.adapter = bookshelfAdapter
+    }
+
+    private fun upRecyclerData() {
+        viewModel.bookGroup?.let {
+            when (it.groupId) {
+
+            }
         }
     }
 
