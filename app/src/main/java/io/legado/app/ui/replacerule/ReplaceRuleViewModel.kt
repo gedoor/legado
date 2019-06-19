7997995936326abd2370d1e31b236b3b4ec7375f
gedoor <gekunfei@live.com>
2019-06-19 19:15:29 +0800

    up

---
+++ app/src/main/java/io/legado/app/ui/replacerule/ReplaceRuleViewModel.kt
@@ -0,0 +1,8 @@
+package io.legado.app.ui.replacerule
+
+import android.app.Application
+import io.legado.app.base.BaseViewModel
+
+class ReplaceRuleViewModel(application: Application) : BaseViewModel(application) {
+
+}
