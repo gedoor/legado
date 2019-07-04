6209fde93e93f93a8e88b30c781d3bbbc008b746
kunfei <gekunfei@live.com>
2019-07-04 14:58:54 +0800

    up

--- app/src/main/java/io/legado/app/data/dao/BookChapterDao.kt
+++ app/src/main/java/io/legado/app/data/dao/BookChapterDao.kt
@@ -1,9 +1,11 @@
 package io.legado.app.data.dao
 
+import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import io.legado.app.data.entities.BookChapter
 
+@Dao
 interface BookChapterDao {
 
 
