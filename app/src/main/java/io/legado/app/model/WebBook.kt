2f41e23d68636ae25951315a3ded71ec27c81d71
kunfei <gekunfei@live.com>
2019-07-11 23:12:52 +0800

    up

--- app/src/main/java/io/legado/app/model/WebBook.kt
+++ app/src/main/java/io/legado/app/model/WebBook.kt
@@ -3,8 +3,10 @@ package io.legado.app.model
 import io.legado.app.data.api.IHttpGetApi
 import io.legado.app.data.api.IHttpPostApi
 import io.legado.app.data.entities.BookSource
+import io.legado.app.data.entities.SearchBook
 import io.legado.app.help.http.HttpHelper
 import io.legado.app.model.analyzeRule.AnalyzeUrl
+import io.legado.app.model.webbook.BookList
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.MainScope
@@ -14,10 +16,9 @@ class WebBook(val bookSource: BookSource) : CoroutineScope by MainScope() {
 
     fun searchBook(key: String, page: Int?, success: (() -> Unit), error: (() -> Unit)) {
         launch(IO) {
-            val searchRule = bookSource.getSearchRule()
-            searchRule?.searchUrl?.let { searchUrl ->
+            bookSource.getSearchRule().searchUrl?.let { searchUrl ->
                 val analyzeUrl = AnalyzeUrl(searchUrl)
-                val res = when {
+                val response = when {
                     analyzeUrl.method == AnalyzeUrl.Method.POST -> HttpHelper.getApiService<IHttpPostApi>(
                         analyzeUrl.baseUrl
                     ).postBody(
@@ -31,7 +32,7 @@ class WebBook(val bookSource: BookSource) : CoroutineScope by MainScope() {
                     else -> HttpHelper.getApiService<IHttpGetApi>(analyzeUrl.baseUrl)
                         .getMap(analyzeUrl.url, analyzeUrl.fieldMap, analyzeUrl.headerMap).await()
                 }
-
+                val bookList = BookList().analyzeBookList(response, bookSource)
             }
 
         }
--- app/src/main/java/io/legado/app/model/webbook/BookList.kt
+++ app/src/main/java/io/legado/app/model/webbook/BookList.kt
@@ -1,10 +1,12 @@
 package io.legado.app.model.webbook
 
+import io.legado.app.data.entities.BookSource
 import io.legado.app.data.entities.SearchBook
+import retrofit2.Response
 
 class BookList {
 
-    fun analyzeBookList(): ArrayList<SearchBook> {
+    fun analyzeBookList(response: Response<String>, bookSource: BookSource): ArrayList<SearchBook> {
         var bookList = ArrayList<SearchBook>()
 
         return bookList
