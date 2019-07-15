a5892e25408c7e892299ebdb437df589eb3029dc
kunfei <gekunfei@live.com>
2019-07-15 16:20:40 +0800

    up

--- app/src/main/java/io/legado/app/model/webbook/BookInfo.kt
+++ app/src/main/java/io/legado/app/model/webbook/BookInfo.kt
@@ -24,6 +24,7 @@ object BookInfo {
             )
         )
 
+
     }
 
 }
--- app/src/main/java/io/legado/app/model/webbook/BookList.kt
+++ app/src/main/java/io/legado/app/model/webbook/BookList.kt
@@ -82,6 +82,9 @@ object BookList {
                     }
                 }
             }
+            if (reverse) {
+                bookList.reverse()
+            }
         }
         return bookList
     }
