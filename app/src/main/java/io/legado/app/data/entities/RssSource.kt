e8219eeb10fb4645bd25086743479f9366ce415b
kunfei <gekunfei@live.com>
2019-08-03 21:59:32 +0800

    up

--- app/src/main/java/io/legado/app/data/entities/RssSource.kt
+++ app/src/main/java/io/legado/app/data/entities/RssSource.kt
@@ -2,11 +2,14 @@ package io.legado.app.data.entities
 
 import android.os.Parcelable
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 @Entity(tableName = "rssSources")
 data class RssSource(
     var sourceName: String,
+    @PrimaryKey
+    var sourceUrl: String,
     var enabled: Boolean = true
 ) : Parcelable
