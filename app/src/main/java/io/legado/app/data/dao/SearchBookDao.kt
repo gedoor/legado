7f070f2a603cc52bdc186571ede06b153ca34509
kunfei <gekunfei@live.com>
2019-08-04 19:59:59 +0800

    up

--- app/src/main/java/io/legado/app/data/dao/SearchBookDao.kt
+++ app/src/main/java/io/legado/app/data/dao/SearchBookDao.kt
@@ -13,7 +13,8 @@ interface SearchBookDao {
     @Query("SELECT * FROM searchBooks")
     fun observeAll(): DataSource.Factory<Int, SearchBook>
 
-    @Query("SELECT name, author, '' bookUrl, '' origin, '' originName, count(origin) originCount, min(time) time, max(intro) intro, max(kind) kind, max(coverUrl) coverUrl, max(latestChapterTitle) latestChapterTitle FROM searchBooks where time >= :time group by name and author order by searchOrder and originOrder and time")
+    //    @Query("SELECT name, author, '' bookUrl, '' origin, '' originName, count(origin) originCount, min(time) time, max(intro) intro, max(kind) kind, max(coverUrl) coverUrl, max(latestChapterTitle) latestChapterTitle FROM searchBooks where time >= :time group by name and author order by searchOrder and originOrder and time")
+    @Query("SELECT * FROM searchBooks")
     fun observeNew(time: Long): DataSource.Factory<Int, SearchBook>
 
 
