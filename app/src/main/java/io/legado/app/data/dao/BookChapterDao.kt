1f5de3bd2ce8d8976bd39f6401dafa3b571fc696
kunfei <gekunfei@live.com>
2019-08-06 17:09:27 +0800

    up

--- app/src/main/java/io/legado/app/data/dao/BookChapterDao.kt
+++ app/src/main/java/io/legado/app/data/dao/BookChapterDao.kt
@@ -1,5 +1,6 @@
 package io.legado.app.data.dao
 
+import androidx.paging.DataSource
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
@@ -8,6 +9,9 @@ import io.legado.app.data.entities.BookChapter
 
 @Dao
 interface BookChapterDao {
+
+    @Query("select * from chapters where bookUrl = :bookUrl")
+    fun observeByBook(bookUrl: String): DataSource.Factory<Int, BookChapter>
 
     @Query("select * from chapters where bookUrl = :bookUrl and `index` = :index")
     fun getChapter(bookUrl: String, index: Int): BookChapter?
