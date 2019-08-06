d9392769216e6391c40c00c07d13714896579ed9
kunfei <gekunfei@live.com>
2019-08-06 22:26:46 +0800

    up

---
+++ app/src/main/java/io/legado/app/ui/chapterlist/ChapterListActivity.kt
@@ -0,0 +1,13 @@
+package io.legado.app.ui.chapterlist
+
+import android.os.Bundle
+import io.legado.app.R
+import io.legado.app.base.BaseActivity
+
+class ChapterListActivity : BaseActivity(R.layout.activity_chapter_list) {
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+
+    }
+
+}
