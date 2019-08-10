68aeaa5ecb40acbd400e03a4362f9f0b97e3ce1f
kunfei <gekunfei@live.com>
2019-08-10 10:35:04 +0800

    up

--- app/src/main/java/io/legado/app/help/BookHelp.kt
+++ app/src/main/java/io/legado/app/help/BookHelp.kt
@@ -21,7 +21,10 @@ object BookHelp {
 
 
     fun formatAuthor(author: String?): String {
-        return author?.replace("作\\s*者[\\s:：]*".toRegex(), "")?.replace("\\s+".toRegex(), " ")?.trim { it <= ' ' }
+        return author
+            ?.replace("作\\s*者[\\s:：]*".toRegex(), "")
+            ?.replace("\\s+".toRegex(), " ")
+            ?.trim { it <= ' ' }
             ?: ""
     }
 
--- app/src/main/java/io/legado/app/ui/read/ReadViewModel.kt
+++ app/src/main/java/io/legado/app/ui/read/ReadViewModel.kt
@@ -1,8 +1,20 @@
 package io.legado.app.ui.read
 
 import android.app.Application
+import android.content.Intent
+import androidx.lifecycle.MutableLiveData
 import io.legado.app.base.BaseViewModel
+import io.legado.app.data.entities.Book
 
 class ReadViewModel(application: Application) : BaseViewModel(application) {
+
+    val bookData = MutableLiveData<Book>()
+
+    fun initData(intent: Intent) {
+        val bookUrl = intent.getStringExtra("bookUrl")
+        if (!bookUrl.isNullOrEmpty()) {
+
+        }
+    }
 
 }
