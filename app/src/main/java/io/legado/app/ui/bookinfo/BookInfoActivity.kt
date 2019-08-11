433440609ea86527a5c26f02ec0e778ebbd7b6b6
kunfei <gekunfei@live.com>
2019-08-11 17:21:29 +0800

    up

--- app/src/main/java/io/legado/app/ui/bookinfo/BookInfoActivity.kt
+++ app/src/main/java/io/legado/app/ui/bookinfo/BookInfoActivity.kt
@@ -2,9 +2,12 @@ package io.legado.app.ui.bookinfo
 
 import android.os.Bundle
 import io.legado.app.R
-import io.legado.app.base.BaseActivity
+import io.legado.app.base.VMBaseActivity
+import io.legado.app.utils.getViewModel
 
-class BookInfoActivity : BaseActivity(R.layout.activity_book_info) {
+class BookInfoActivity : VMBaseActivity<BookInfoViewModel>(R.layout.activity_book_info) {
+    override val viewModel: BookInfoViewModel
+        get() = getViewModel(BookInfoViewModel::class.java)
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
 
---
+++ app/src/main/java/io/legado/app/ui/bookinfo/BookInfoViewModel.kt
@@ -0,0 +1,9 @@
+package io.legado.app.ui.bookinfo
+
+import android.app.Application
+import io.legado.app.base.BaseViewModel
+
+class BookInfoViewModel(application: Application) : BaseViewModel(application) {
+
+
+}
