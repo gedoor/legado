17414beeef95edbd9077b50c3eb71766226b6514
kunfei <gekunfei@live.com>
2019-08-14 11:56:11 +0800

    up

--- app/src/main/java/io/legado/app/data/entities/BookChapter.kt
+++ app/src/main/java/io/legado/app/data/entities/BookChapter.kt
@@ -9,24 +9,24 @@ import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 @Entity(
-        tableName = "chapters",
-        primaryKeys = ["url", "bookUrl"],
+    tableName = "chapters",
+    primaryKeys = ["url", "bookUrl"],
     indices = [(Index(value = ["bookUrl"], unique = false)), (Index(value = ["bookUrl", "index"], unique = true))],
-        foreignKeys = [(ForeignKey(
-                entity = Book::class,
-                parentColumns = ["bookUrl"],
-                childColumns = ["bookUrl"],
-                onDelete = ForeignKey.CASCADE
-        ))]
+    foreignKeys = [(ForeignKey(
+        entity = Book::class,
+        parentColumns = ["bookUrl"],
+        childColumns = ["bookUrl"],
+        onDelete = ForeignKey.CASCADE
+    ))]
 )    // 删除书籍时自动删除章节
 data class BookChapter(
-        var url: String = "",               // 章节地址
-        var title: String = "",              // 章节标题
-        var bookUrl: String = "",           // 书籍地址
-        var index: Int = 0,                 // 章节序号
-        var resourceUrl: String? = null,    // 音频真实URL
-        var tag: String? = null,            //
-        var start: Long? = null,            // 章节起始位置
-        var end: Long? = null               // 章节终止位置
+    var url: String = "",               // 章节地址
+    var title: String = "",              // 章节标题
+    var bookUrl: String = "",           // 书籍地址
+    var index: Int = 0,                 // 章节序号
+    var resourceUrl: String? = null,    // 音频真实URL
+    var tag: String? = null,            //
+    var start: Long? = null,            // 章节起始位置
+    var end: Long? = null               // 章节终止位置
 ) : Parcelable
 
