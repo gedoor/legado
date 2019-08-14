548c94b775c14e3686dab1ff9d8ef2c07493c482
kunfei <gekunfei@live.com>
2019-08-14 17:41:35 +0800

    up

--- app/src/main/java/io/legado/app/ui/changesource/ChangeSourceDialog.kt
+++ app/src/main/java/io/legado/app/ui/changesource/ChangeSourceDialog.kt
@@ -1,2 +1,22 @@
 package io.legado.app.ui.changesource
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import io.legado.app.R
+
+
+class ChangeSourceDialog : DialogFragment() {
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.dialog_change_source, container)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
