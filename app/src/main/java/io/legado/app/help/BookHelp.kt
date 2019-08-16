a399a32b3ab94024dafbbc77f0a06effdcbb9110
kunfei <gekunfei@live.com>
2019-08-16 11:39:17 +0800

    up

--- app/src/main/java/io/legado/app/help/BookHelp.kt
+++ app/src/main/java/io/legado/app/help/BookHelp.kt
@@ -1,6 +1,25 @@
 package io.legado.app.help
 
+import io.legado.app.data.entities.Book
+import io.legado.app.data.entities.BookChapter
+
 object BookHelp {
+
+    fun saveContent(book: Book, bookChapter: BookChapter, content: String) {
+
+    }
+
+    fun hasContent(book: Book, bookChapter: BookChapter): Boolean {
+
+
+        return false
+    }
+
+    fun getContent(book: Book, bookChapter: BookChapter): String? {
+
+
+        return null
+    }
 
     fun formatAuthor(author: String?): String {
         return author
