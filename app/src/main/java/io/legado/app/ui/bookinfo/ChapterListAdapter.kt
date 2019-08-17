8551379fd801193a0a0d35126764d528a2aeb9fe
kunfei <gekunfei@live.com>
2019-08-18 07:59:06 +0800

    up

--- app/src/main/java/io/legado/app/ui/bookinfo/ChapterListAdapter.kt
+++ app/src/main/java/io/legado/app/ui/bookinfo/ChapterListAdapter.kt
@@ -6,13 +6,21 @@ import io.legado.app.base.adapter.ItemViewHolder
 import io.legado.app.base.adapter.SimpleRecyclerAdapter
 import io.legado.app.data.entities.BookChapter
 import kotlinx.android.synthetic.main.item_chapter_list.view.*
+import org.jetbrains.anko.sdk27.listeners.onClick
 
 class ChapterListAdapter(context: Context) : SimpleRecyclerAdapter<BookChapter>(context, R.layout.item_chapter_list) {
+    var callBack: CallBack? = null
 
     override fun convert(holder: ItemViewHolder, item: BookChapter, payloads: MutableList<Any>) {
         holder.itemView.apply {
             tv_chapter_name.text = item.title
+            this.onClick {
+                callBack?.skipToChapter(item.index)
+            }
         }
     }
 
+    interface CallBack {
+        fun skipToChapter(index: Int)
+    }
 }
