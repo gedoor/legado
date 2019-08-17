22b1238bd2b43db87391bea0484ce2984c069cf8
kunfei <gekunfei@live.com>
2019-08-17 14:57:21 +0800

    up

--- app/src/main/java/io/legado/app/ui/widget/page/TextChapter.kt
+++ app/src/main/java/io/legado/app/ui/widget/page/TextChapter.kt
@@ -1,3 +1,15 @@
 package io.legado.app.ui.widget.page
 
-data class TextChapter(val position: Int, val title: String, val pages: List<TextPage>)
+data class TextChapter(
+    val position: Int,
+    val title: String,
+    val pages: List<TextPage>
+) {
+    fun getPage(index: Int): TextPage? {
+        if (index >= 0 && index < pages.size) {
+            return pages[index]
+        }
+        return null
+    }
+}
+
