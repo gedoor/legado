6a85aad6844d2407afc2652fd707675d6aca225d
kunfei <gekunfei@live.com>
2019-08-17 22:44:08 +0800

    up

--- app/src/main/java/io/legado/app/utils/GsonExtensions.kt
+++ app/src/main/java/io/legado/app/utils/GsonExtensions.kt
@@ -2,11 +2,15 @@ package io.legado.app.utils
 
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
-import com.google.gson.JsonParser
 import com.google.gson.reflect.TypeToken
 import org.jetbrains.anko.attempt
 
-val GSON: Gson by lazy { GsonBuilder().create() }
+val GSON: Gson by lazy {
+    GsonBuilder()
+        .disableHtmlEscaping()
+        .setPrettyPrinting()
+        .create()
+}
 
 inline fun <reified T> genericType() = object : TypeToken<T>() {}.type
 
