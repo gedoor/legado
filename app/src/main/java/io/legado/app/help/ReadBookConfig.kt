507df7f39d4c8f944df398b6f951cbbe140eab00
kunfei <gekunfei@live.com>
2019-08-20 21:46:18 +0800

    up

--- app/src/main/java/io/legado/app/help/ReadBookConfig.kt
+++ app/src/main/java/io/legado/app/help/ReadBookConfig.kt
@@ -3,12 +3,19 @@ package io.legado.app.help
 import android.graphics.drawable.Drawable
 import com.jayway.jsonpath.JsonPath
 import io.legado.app.App
-import io.legado.app.utils.readInt
+import io.legado.app.utils.GSON
+import io.legado.app.utils.getPrefInt
+import io.legado.app.utils.putPrefInt
+import java.io.BufferedWriter
 import java.io.File
+import java.io.FileWriter
+import java.io.IOException
 
 object ReadBookConfig {
     private val configList = arrayListOf<Config>()
-    var styleSelect = 0
+    var styleSelect
+        get() = App.INSTANCE.getPrefInt("readStyleSelect")
+        set(value) = App.INSTANCE.putPrefInt("readStyleSelect", value)
     var bg: Drawable? = null
 
     init {
@@ -16,17 +23,34 @@ object ReadBookConfig {
         val json = if (configFile.exists()) {
             String(configFile.readBytes())
         } else {
-            String(App.INSTANCE.assets.open("defaultConfig.json").readBytes())
+            String(App.INSTANCE.assets.open("readConfig.json").readBytes())
         }
         JsonPath.parse(json).let {
-            styleSelect = it.readInt("$.readBookSelect") ?: 0
             configList.clear()
-            configList.addAll(it.read<Array<Config>>("$.readBook"))
+            configList.addAll(it.read<Array<Config>>("$"))
         }
     }
 
     fun getConfig(): Config {
         return configList[styleSelect]
+    }
+
+    fun save() {
+        val json = GSON.toJson(configList)
+        val configFile = File(App.INSTANCE.filesDir.absolutePath + File.separator + "config")
+        //获取流并存储
+        try {
+            BufferedWriter(FileWriter(configFile)).use { writer ->
+                writer.write(json)
+                writer.flush()
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+
+    fun reset() {
+
     }
 
     data class Config(
