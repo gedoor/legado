9aa2735767bf65c3ce282dba8729175acb8917f8
kunfei <gekunfei@live.com>
2019-09-01 22:54:52 +0800

    up

--- app/src/main/java/io/legado/app/ui/replacerule/ReplaceRuleViewModel.kt
+++ app/src/main/java/io/legado/app/ui/replacerule/ReplaceRuleViewModel.kt
@@ -1,9 +1,11 @@
 package io.legado.app.ui.replacerule
 
 import android.app.Application
+import android.text.TextUtils
 import io.legado.app.App
 import io.legado.app.base.BaseViewModel
 import io.legado.app.data.entities.ReplaceRule
+import io.legado.app.utils.splitNotBlank
 
 class ReplaceRuleViewModel(application: Application) : BaseViewModel(application) {
 
@@ -20,4 +22,43 @@ class ReplaceRuleViewModel(application: Application) : BaseViewModel(application
         }
     }
 
+    fun addGroup(group: String) {
+        execute {
+            val sources = App.db.replaceRuleDao().noGroup
+            sources.map { source ->
+                source.group = group
+            }
+            App.db.replaceRuleDao().update(*sources.toTypedArray())
+        }
+    }
+
+    fun upGroup(oldGroup: String, newGroup: String?) {
+        execute {
+            val sources = App.db.replaceRuleDao().getByGroup(oldGroup)
+            sources.map { source ->
+                source.group?.splitNotBlank(",")?.toHashSet()?.let {
+                    it.remove(oldGroup)
+                    if (!newGroup.isNullOrEmpty())
+                        it.add(newGroup)
+                    source.group = TextUtils.join(",", it)
+                }
+            }
+            App.db.replaceRuleDao().update(*sources.toTypedArray())
+        }
+    }
+
+    fun delGroup(group: String) {
+        execute {
+            execute {
+                val sources = App.db.replaceRuleDao().getByGroup(group)
+                sources.map { source ->
+                    source.group?.splitNotBlank(",")?.toHashSet()?.let {
+                        it.remove(group)
+                        source.group = TextUtils.join(",", it)
+                    }
+                }
+                App.db.replaceRuleDao().update(*sources.toTypedArray())
+            }
+        }
+    }
 }
