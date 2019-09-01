00e5da3683e0604ab8f375daf0aa2a15afc82c3e
kunfei <gekunfei@live.com>
2019-09-01 12:23:18 +0800

    up

---
+++ app/src/main/java/io/legado/app/web/WebSocketServer.kt
@@ -0,0 +1,13 @@
+package io.legado.app.web
+
+import fi.iki.elonen.NanoWSD
+import io.legado.app.web.controller.SourceDebugWebSocket
+
+class WebSocketServer(port: Int) : NanoWSD(port) {
+
+    override fun openWebSocket(handshake: IHTTPSession): WebSocket? {
+        return if (handshake.uri == "/sourceDebug") {
+            SourceDebugWebSocket(handshake)
+        } else null
+    }
+}
