70fe90f20ccf532b9b4216619d7b9163301923dc
kunfei <gekunfei@live.com>
2019-09-03 19:40:08 +0800

    up

--- app/src/main/java/io/legado/app/ui/readrss/ReadRssActivity.kt
+++ app/src/main/java/io/legado/app/ui/readrss/ReadRssActivity.kt
@@ -8,7 +8,7 @@ import io.legado.app.base.BaseActivity
 class ReadRssActivity : BaseActivity(R.layout.activity_read_rss) {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
     }
 
 
