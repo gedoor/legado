ca2ca0caa5d0a612ef291081a6e7dfa83febd5f3
kunfei <gekunfei@live.com>
2019-09-07 17:31:05 +0800

    up

--- app/src/main/java/io/legado/app/base/BaseActivity.kt
+++ app/src/main/java/io/legado/app/base/BaseActivity.kt
@@ -77,10 +77,13 @@ abstract class BaseActivity(private val layoutID: Int, private val fullScreen: B
 
     private fun setupSystemBar() {
         if (fullScreen) {
-            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS or WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION)
+            window.clearFlags(
+                WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
+                        or WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION
+            )
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
             window.decorView.systemUiVisibility =
                 View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
         }
         ATH.setStatusbarColorAuto(this, fullScreen)
     }
