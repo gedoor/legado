cd6c1d9c49dd78bc5d42e41fd045252348233e68
kunfei <gekunfei@live.com>
2019-09-11 16:46:12 +0800

    up

--- app/src/main/java/io/legado/app/service/HttpReadAloudService.kt
+++ app/src/main/java/io/legado/app/service/HttpReadAloudService.kt
@@ -2,20 +2,28 @@ package io.legado.app.service
 
 class HttpReadAloudService : BaseReadAloudService() {
 
-    override fun newReadAloud(dataKey: String?, play: Boolean) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    override fun play() {
+
     }
 
     override fun upSpeechRate(reset: Boolean) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
     }
 
     override fun prevP() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
     }
 
     override fun nextP() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
     }
 
 }
