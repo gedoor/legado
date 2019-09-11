803f81e8cf016d28a811505adf543b6d0d1c2781
kunfei <gekunfei@live.com>
2019-09-11 14:16:21 +0800

    up

--- app/src/main/java/io/legado/app/lib/theme/view/ATEAccentBgTextView.kt
+++ app/src/main/java/io/legado/app/lib/theme/view/ATEAccentBgTextView.kt
@@ -4,25 +4,21 @@ import android.content.Context
 import android.graphics.Color
 import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatTextView
+import io.legado.app.R
 import io.legado.app.lib.theme.ColorUtils
 import io.legado.app.lib.theme.Selector
 import io.legado.app.lib.theme.ThemeStore
-import io.legado.app.utils.dp
 
-class ATEAccentBgTextView : AppCompatTextView {
-    constructor(context: Context) : super(context)
-
-    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
-
-    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(
-        context,
-        attrs,
-        defStyleAttr
-    )
+class ATEAccentBgTextView(context: Context, attrs: AttributeSet) :
+    AppCompatTextView(context, attrs) {
 
     init {
+        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.ATEAccentBgTextView)
+        val radios =
+            typedArray.getDimensionPixelOffset(R.styleable.ATEAccentBgTextView_abt_radius, 0)
+        typedArray.recycle()
         background = Selector.shapeBuild()
-            .setCornerRadius(3.dp)
+            .setCornerRadius(radios)
             .setDefaultBgColor(ThemeStore.accentColor(context))
             .setPressedBgColor(ColorUtils.darkenColor(ThemeStore.accentColor(context)))
             .create()
