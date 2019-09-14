80ab179477a25d20ce2689da0b58de51f3b11421
kunfei <gekunfei@live.com>
2019-09-14 11:29:20 +0800

    up

--- app/src/main/java/io/legado/app/help/storage/Backup.kt
+++ app/src/main/java/io/legado/app/help/storage/Backup.kt
@@ -1,5 +1,8 @@
 package io.legado.app.help.storage
 
+import io.legado.app.App
+import io.legado.app.utils.GSON
+
 object Backup {
 
     fun backup() {
@@ -7,6 +10,12 @@ object Backup {
     }
 
     fun autoBackup() {
+
+    }
+
+    private fun backupBookshelf() {
+        val books = App.db.bookDao().allBooks
+        val json = GSON.toJson(books)
 
     }
 }
