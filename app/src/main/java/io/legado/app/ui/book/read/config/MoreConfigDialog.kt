ecddadad7a447ecdc6a51310995ebf64eeb5d44a
kunfei <gekunfei@live.com>
2019-09-14 17:51:51 +0800

    up

--- app/src/main/java/io/legado/app/ui/book/read/config/MoreConfigDialog.kt
+++ app/src/main/java/io/legado/app/ui/book/read/config/MoreConfigDialog.kt
@@ -74,11 +74,15 @@ class MoreConfigDialog : DialogFragment() {
 
         override fun onResume() {
             super.onResume()
-            preferenceManager.sharedPreferences.registerOnSharedPreferenceChangeListener(this)
+            preferenceManager
+                .sharedPreferences
+                .registerOnSharedPreferenceChangeListener(this)
         }
 
         override fun onPause() {
-            preferenceManager.sharedPreferences.unregisterOnSharedPreferenceChangeListener(this)
+            preferenceManager
+                .sharedPreferences
+                .unregisterOnSharedPreferenceChangeListener(this)
             super.onPause()
         }
 
