4b3d4dc84b1bcf53dae7a625dc18fe444034480e
kunfei <gekunfei@live.com>
2019-09-15 22:06:01 +0800

    up

--- app/src/main/java/io/legado/app/ui/rss/source/RssSourceActivity.kt
+++ app/src/main/java/io/legado/app/ui/rss/source/RssSourceActivity.kt
@@ -1,2 +1,18 @@
 package io.legado.app.ui.rss.source
 
+import android.os.Bundle
+import io.legado.app.R
+import io.legado.app.base.VMBaseActivity
+import io.legado.app.utils.getViewModel
+
+
+class RssSourceActivity : VMBaseActivity<RssSourceViewModel>(R.layout.activity_rss_source) {
+    override val viewModel: RssSourceViewModel
+        get() = getViewModel(RssSourceViewModel::class.java)
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+
+    }
+
+
+}
---
+++ app/src/main/java/io/legado/app/ui/rss/source/RssSourceViewModel.kt
@@ -0,0 +1,8 @@
+package io.legado.app.ui.rss.source
+
+import android.app.Application
+import io.legado.app.base.BaseViewModel
+
+class RssSourceViewModel(application: Application) : BaseViewModel(application) {
+
+}
