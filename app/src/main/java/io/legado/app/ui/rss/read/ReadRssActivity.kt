ae7b937cd24932656c75c91188d50f43dbbb6b5a
kunfei <gekunfei@live.com>
2019-10-02 17:46:16 +0800

    up

--- app/src/main/java/io/legado/app/ui/rss/read/ReadRssActivity.kt
+++ app/src/main/java/io/legado/app/ui/rss/read/ReadRssActivity.kt
@@ -1,15 +1,15 @@
 package io.legado.app.ui.rss.read
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import io.legado.app.R
+import io.legado.app.base.BaseActivity
 import kotlinx.android.synthetic.main.activity_rss_read.*
 
-class ReadRssActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_rss_read)
+class ReadRssActivity : BaseActivity(R.layout.activity_rss_read) {
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
         val description = intent.getStringExtra("description")
         webView.loadData("<style>img{max-width:100%}</style>$description","text/html", "utf-8")
     }
+
 }
