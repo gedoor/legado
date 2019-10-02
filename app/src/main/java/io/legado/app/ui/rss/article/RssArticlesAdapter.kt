b5565a96c5926a70cdf3c7dc97a86ced12346117
kunfei <gekunfei@live.com>
2019-10-02 23:04:37 +0800

    up

--- app/src/main/java/io/legado/app/ui/rss/article/RssArticlesAdapter.kt
+++ app/src/main/java/io/legado/app/ui/rss/article/RssArticlesAdapter.kt
@@ -5,6 +5,9 @@ import io.legado.app.R
 import io.legado.app.base.adapter.ItemViewHolder
 import io.legado.app.base.adapter.SimpleRecyclerAdapter
 import io.legado.app.data.entities.RssArticle
+import io.legado.app.help.ImageLoader
+import io.legado.app.utils.gone
+import io.legado.app.utils.visible
 import kotlinx.android.synthetic.main.item_rss_article.view.*
 import org.jetbrains.anko.sdk27.listeners.onClick
 
@@ -18,6 +21,13 @@ class RssArticlesAdapter(context: Context, val callBack: CallBack) :
             tv_pub_date.text = item.pubDate
             onClick {
                 callBack.readRss(item)
+            }
+            if (item.image.isNullOrBlank()) {
+                image_view.gone()
+            } else {
+                image_view.visible()
+                ImageLoader.load(context, item.image)
+                    .setAsBitmap(image_view)
             }
         }
     }
