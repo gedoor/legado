19cb9bd1fe60bc8caf8ee4decbde6b5fbde6bc99
kunfei <gekunfei@live.com>
2019-10-07 10:34:39 +0800

    up

--- app/src/main/java/io/legado/app/ui/rss/read/ReadRssActivity.kt
+++ app/src/main/java/io/legado/app/ui/rss/read/ReadRssActivity.kt
@@ -2,10 +2,14 @@ package io.legado.app.ui.rss.read
 
 import android.os.Bundle
 import io.legado.app.R
-import io.legado.app.base.BaseActivity
+import io.legado.app.base.VMBaseActivity
+import io.legado.app.utils.getViewModel
 import kotlinx.android.synthetic.main.activity_rss_read.*
 
-class ReadRssActivity : BaseActivity(R.layout.activity_rss_read) {
+class ReadRssActivity : VMBaseActivity<ReadRssViewModel>(R.layout.activity_rss_read) {
+
+    override val viewModel: ReadRssViewModel
+        get() = getViewModel(ReadRssViewModel::class.java)
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         val description = intent.getStringExtra("description")
--- app/src/main/java/io/legado/app/ui/rss/read/ReadRssViewModel.kt
+++ app/src/main/java/io/legado/app/ui/rss/read/ReadRssViewModel.kt
@@ -1,2 +1,8 @@
 package io.legado.app.ui.rss.read
 
+import android.app.Application
+import io.legado.app.base.BaseViewModel
+
+class ReadRssViewModel(application: Application) : BaseViewModel(application) {
+
+}
