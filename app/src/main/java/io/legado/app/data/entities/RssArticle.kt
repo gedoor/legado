82435f601c6aa29627a750fca8afaf6e23ac29f4
kunfei <gekunfei@live.com>
2019-10-08 11:55:50 +0800

    up

--- app/src/main/java/io/legado/app/data/entities/RssArticle.kt
+++ app/src/main/java/io/legado/app/data/entities/RssArticle.kt
@@ -2,16 +2,16 @@ package io.legado.app.data.entities
 
 import androidx.room.Entity
 import androidx.room.Ignore
-import androidx.room.PrimaryKey
 
 
-@Entity(tableName = "rssArticles")
+@Entity(
+    tableName = "rssArticles",
+    primaryKeys = ["origin", "title"]
+)
 data class RssArticle(
     var origin: String = "",
-    var time: Long = System.currentTimeMillis(),
-    @PrimaryKey
-    var guid: String = "",
     var title: String? = null,
+    var time: Long = System.currentTimeMillis(),
     var author: String? = null,
     var link: String? = null,
     var pubDate: String? = null,
