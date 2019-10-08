b75760ba5099ab2e26b968b03c4348293dcbdcd0
kunfei <gekunfei@live.com>
2019-10-08 10:32:23 +0800

    up

--- app/src/main/java/io/legado/app/data/entities/RssSource.kt
+++ app/src/main/java/io/legado/app/data/entities/RssSource.kt
@@ -17,7 +17,6 @@ data class RssSource(
     var enabled: Boolean = true,
     //列表规则
     var ruleArticles: String? = null,
-    var ruleGuid: String? = null,
     var ruleTitle: String? = null,
     var ruleAuthor: String? = null,
     var rulePubDate: String? = null,
