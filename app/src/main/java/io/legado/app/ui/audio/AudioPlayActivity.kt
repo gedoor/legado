8c663917037bd9db836cd7372468c8be6109a5b2
kunfei <gekunfei@live.com>
2019-10-26 08:52:14 +0800

    up

--- app/src/main/java/io/legado/app/ui/audio/AudioPlayActivity.kt
+++ app/src/main/java/io/legado/app/ui/audio/AudioPlayActivity.kt
@@ -4,13 +4,15 @@ import android.os.Bundle
 import io.legado.app.R
 import io.legado.app.base.VMBaseActivity
 import io.legado.app.utils.getViewModel
+import kotlinx.android.synthetic.main.view_title_bar.*
 
 class AudioPlayActivity : VMBaseActivity<AudioPlayViewModel>(R.layout.activity_audio_play) {
     override val viewModel: AudioPlayViewModel
         get() = getViewModel(AudioPlayViewModel::class.java)
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
-
+        setSupportActionBar(toolbar)
+        viewModel.initData(intent)
     }
 
 
