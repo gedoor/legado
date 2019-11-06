e2a74af52bea822ac2334a2d9e0c8fc63b263442
kunfei <gekunfei@live.com>
2019-11-06 13:16:58 +0800

    up

--- app/src/main/java/io/legado/app/data/entities/rule/ContentRule.kt
+++ app/src/main/java/io/legado/app/data/entities/rule/ContentRule.kt
@@ -3,5 +3,6 @@ package io.legado.app.data.entities.rule
 data class ContentRule(
     var content: String? = null,
     var nextContentUrl: String? = null,
-    var webJs: String? = null
+    var webJs: String? = null,
+    var sourceRegex: String? = null
 )
