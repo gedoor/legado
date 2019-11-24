08de132d09b90920d5d2038c8e11878b5b5ea09d
kunfei <gekunfei@live.com>
2019-11-24 19:41:04 +0800

    up

--- app/src/main/java/io/legado/app/ui/widget/BadgeView.kt
+++ app/src/main/java/io/legado/app/ui/widget/BadgeView.kt
@@ -23,7 +23,7 @@ import io.legado.app.utils.visible
 /**
  * Created by milad heydari on 5/6/2016.
  */
-class BadgeView(
+class BadgeView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyle: Int = android.R.attr.textViewStyle
