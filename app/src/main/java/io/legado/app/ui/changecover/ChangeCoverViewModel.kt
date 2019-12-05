182e1fd11588843fe3ca20c7811d49420016fd89
kunfei <gekunfei@live.com>
2019-12-05 09:44:46 +0800

    up

---
+++ app/src/main/java/io/legado/app/ui/changecover/ChangeCoverViewModel.kt
@@ -0,0 +1,9 @@
+package io.legado.app.ui.changecover
+
+import android.app.Application
+import io.legado.app.base.BaseViewModel
+
+class ChangeCoverViewModel(application: Application) : BaseViewModel(application) {
+
+
+}
