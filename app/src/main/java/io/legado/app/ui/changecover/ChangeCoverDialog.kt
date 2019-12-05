e6776a4c2a604c02a09527a8fc38373116a5f317
kunfei <gekunfei@live.com>
2019-12-05 09:44:27 +0800

    up

--- app/src/main/java/io/legado/app/ui/changecover/ChangeCoverDialog.kt
+++ app/src/main/java/io/legado/app/ui/changecover/ChangeCoverDialog.kt
@@ -1,17 +1,33 @@
 package io.legado.app.ui.changecover
 
+import android.os.Bundle
 import android.util.DisplayMetrics
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
+import io.legado.app.R
+import io.legado.app.utils.getViewModel
 
 
 class ChangeCoverDialog : DialogFragment() {
 
+    private lateinit var viewModel: ChangeCoverViewModel
 
     override fun onStart() {
         super.onStart()
         val dm = DisplayMetrics()
         activity?.windowManager?.defaultDisplay?.getMetrics(dm)
         dialog?.window?.setLayout((dm.widthPixels * 0.9).toInt(), (dm.heightPixels * 0.9).toInt())
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        viewModel = getViewModel(ChangeCoverViewModel::class.java)
+        return inflater.inflate(R.layout.dialog_change_source, container)
     }
 
 
