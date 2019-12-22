4d2d4e5f4248de6e20ac8b8924ee9e351c78b60f
kunfei <gekunfei@live.com>
2019-12-22 21:30:53 +0800

    up

--- app/src/main/java/io/legado/app/ui/download/DownloadActivity.kt
+++ app/src/main/java/io/legado/app/ui/download/DownloadActivity.kt
@@ -1,16 +1,24 @@
 package io.legado.app.ui.download
 
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import io.legado.app.R
 import io.legado.app.base.BaseActivity
+import kotlinx.android.synthetic.main.activity_download.*
 
 
 class DownloadActivity : BaseActivity(R.layout.activity_download) {
 
+    lateinit var adapter: DownloadAdapter
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
-
+        initRecyclerView()
     }
 
+    private fun initRecyclerView() {
+        recycler_view.layoutManager = LinearLayoutManager(this)
+        adapter = DownloadAdapter(this)
+        recycler_view.adapter = adapter
+    }
 
 }
