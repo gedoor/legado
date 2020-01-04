676bfea8f7aaacd0d967445f2aee14dd54ccf054
kunfei <gekunfei@live.com>
2020-01-04 17:18:27 +0800

    up

--- app/src/main/java/io/legado/app/ui/widget/font/FontSelectDialog.kt
+++ app/src/main/java/io/legado/app/ui/widget/font/FontSelectDialog.kt
@@ -101,6 +101,9 @@ class FontSelectDialog : DialogFragment(),
                 }
                 dismiss()
             }
+            R.id.menu_other -> {
+                openFolder()
+            }
         }
         return true
     }
