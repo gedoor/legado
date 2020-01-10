91a5def2cc21e59ebb051277cff64d42daa8be33
kunfei <gekunfei@live.com>
2020-01-10 21:09:30 +0800

    up

---
+++ app/src/main/java/io/legado/app/ui/importbook/ImportBookViewModel.kt
@@ -0,0 +1,10 @@
+package io.legado.app.ui.importbook
+
+import android.app.Application
+import io.legado.app.base.BaseViewModel
+
+
+class ImportBookViewModel(application: Application) : BaseViewModel(application) {
+
+
+}
