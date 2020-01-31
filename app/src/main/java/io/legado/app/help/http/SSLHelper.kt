f4109a0c4b2484222cfb92d04f0109deb478db43
kunfei <gekunfei@live.com>
2020-01-31 21:18:11 +0800

    优化

--- app/src/main/java/io/legado/app/help/http/SSLHelper.kt
+++ app/src/main/java/io/legado/app/help/http/SSLHelper.kt
@@ -14,11 +14,9 @@ import javax.net.ssl.*
 
 object SSLHelper {
 
-    val sslSocketFactory: SSLParams?
-        get() = getSslSocketFactoryBase(null, null, null)
-
     /**
-     * 为了解决客户端不信任服务器数字证书的问题，网络上大部分的解决方案都是让客户端不对证书做任何检查，
+     * 为了解决客户端不信任服务器数字证书的问题，
+     * 网络上大部分的解决方案都是让客户端不对证书做任何检查，
      * 这是一种有很大安全漏洞的办法
      */
     val unsafeTrustManager: X509TrustManager = object : X509TrustManager {
