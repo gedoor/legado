334c5a0bfb3b5a5198c679a9014f237935f41001
kunfei <gekunfei@live.com>
2020-02-03 13:07:16 +0800

    优化

--- app/src/main/java/io/legado/app/ui/config/OtherConfigFragment.kt
+++ app/src/main/java/io/legado/app/ui/config/OtherConfigFragment.kt
@@ -188,6 +188,7 @@ class OtherConfigFragment : PreferenceFragmentCompat(),
                         Intent.FLAG_GRANT_READ_URI_PERMISSION or Intent.FLAG_GRANT_WRITE_URI_PERMISSION
                     )
                     putPrefString(PreferKey.downloadPath, uri.toString())
+                    BookHelp.upDownloadPath()
                 }
             }
         }
