6c9a63fb097a589b8a99985241f662b4a71c93bd
kunfei <gekunfei@live.com>
2020-02-04 21:12:42 +0800

    优化

---
+++ app/src/main/java/io/legado/app/ui/rss/favorites/RssFavoritesActivity.kt
@@ -0,0 +1,16 @@
+package io.legado.app.ui.rss.favorites
+
+import android.os.Bundle
+import io.legado.app.R
+import io.legado.app.base.BaseActivity
+
+
+class RssFavoritesActivity : BaseActivity(R.layout.activity_rss_favorites) {
+
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+
+    }
+
+
+}
