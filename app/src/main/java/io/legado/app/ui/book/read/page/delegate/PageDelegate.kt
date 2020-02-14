948e87a3e785d239b6908fd5e5467c32a488ede8
kunfei <gekunfei@live.com>
2020-02-14 19:49:03 +0800

    feat: 优化代码

--- app/src/main/java/io/legado/app/ui/book/read/page/delegate/PageDelegate.kt
+++ app/src/main/java/io/legado/app/ui/book/read/page/delegate/PageDelegate.kt
@@ -321,4 +321,21 @@ abstract class PageDelegate(protected val pageView: PageView) {
         }
         return hasNext
     }
+
+    fun MotionEvent.toAction(action: Int): MotionEvent {
+        return MotionEvent.obtain(
+            downTime,
+            eventTime,
+            action,
+            x,
+            y,
+            pressure,
+            size,
+            metaState,
+            xPrecision,
+            yPrecision,
+            deviceId,
+            edgeFlags
+        )
+    }
 }
