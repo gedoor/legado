ab8f9fe3ca939fe17dff691fea32d6835d71a999
kunfei <gekunfei@live.com>
2020-02-15 18:05:30 +0800

    feat: 优化代码

--- app/src/main/java/io/legado/app/ui/widget/LabelsBar.kt
+++ app/src/main/java/io/legado/app/ui/widget/LabelsBar.kt
@@ -3,7 +3,6 @@ package io.legado.app.ui.widget
 import android.content.Context
 import android.util.AttributeSet
 import android.widget.LinearLayout
-import androidx.core.view.setPadding
 import io.legado.app.ui.widget.text.AccentBgTextView
 import io.legado.app.utils.dp
 
@@ -25,10 +24,17 @@ class LabelsBar(context: Context, attrs: AttributeSet?) : LinearLayout(context, 
         }
     }
 
+    fun clear() {
+        removeAllViews()
+    }
+
     fun addLabel(label: String) {
         addView(AccentBgTextView(context, null).apply {
-            setPadding(2.dp)
+            setPadding(3.dp, 0, 3.dp, 0)
             setRadios(2)
+            val lp = LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT)
+            lp.setMargins(0, 0, 2.dp, 0)
+            layoutParams = lp
             text = label
         })
     }
