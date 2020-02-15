a9532cf248438a16eb23db0275a8832e8a0d58c6
kunfei <gekunfei@live.com>
2020-02-15 20:20:22 +0800

    feat: 优化代码

--- app/src/main/java/io/legado/app/ui/widget/text/AccentBgTextView.kt
+++ app/src/main/java/io/legado/app/ui/widget/text/AccentBgTextView.kt
@@ -9,6 +9,7 @@ import io.legado.app.lib.theme.ColorUtils
 import io.legado.app.lib.theme.Selector
 import io.legado.app.lib.theme.ThemeStore
 import io.legado.app.utils.dp
+import io.legado.app.utils.getCompatColor
 
 class AccentBgTextView(context: Context, attrs: AttributeSet?) :
     AppCompatTextView(context, attrs) {
@@ -29,10 +30,18 @@ class AccentBgTextView(context: Context, attrs: AttributeSet?) :
     }
 
     private fun upBackground() {
-        background = Selector.shapeBuild()
-            .setCornerRadius(radios)
-            .setDefaultBgColor(ThemeStore.accentColor(context))
-            .setPressedBgColor(ColorUtils.darkenColor(ThemeStore.accentColor(context)))
-            .create()
+        background = if (isInEditMode) {
+            Selector.shapeBuild()
+                .setCornerRadius(radios)
+                .setDefaultBgColor(context.getCompatColor(R.color.colorAccent))
+                .setPressedBgColor(ColorUtils.darkenColor(context.getCompatColor(R.color.colorAccent)))
+                .create()
+        } else {
+            Selector.shapeBuild()
+                .setCornerRadius(radios)
+                .setDefaultBgColor(ThemeStore.accentColor(context))
+                .setPressedBgColor(ColorUtils.darkenColor(ThemeStore.accentColor(context)))
+                .create()
+        }
     }
 }
