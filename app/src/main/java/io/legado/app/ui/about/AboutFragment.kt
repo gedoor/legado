e478e11f22308410f4335d15dfb5f1462239f5a7
kunfei <gekunfei@live.com>
2020-02-16 20:32:16 +0800

    feat: 优化代码

--- app/src/main/java/io/legado/app/ui/about/AboutFragment.kt
+++ app/src/main/java/io/legado/app/ui/about/AboutFragment.kt
@@ -97,7 +97,8 @@ class AboutFragment : PreferenceFragmentCompat() {
         val intent = Intent()
         intent.data =
             Uri.parse("mqqopensdkapi://bizAgent/qm/qr?url=http%3A%2F%2Fqm.qq.com%2Fcgi-bin%2Fqm%2Fqr%3Ffrom%3Dapp%26p%3Dandroid%26k%3D$key")
-        // 此Flag可根据具体产品需要自定义，如设置，则在加群界面按返回，返回手Q主界面，不设置，按返回会返回到呼起产品界面    //intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        // 此Flag可根据具体产品需要自定义，如设置，则在加群界面按返回，返回手Q主界面，不设置，按返回会返回到呼起产品界面
+        // intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
         return try {
             startActivity(intent)
             false
