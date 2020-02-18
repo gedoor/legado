872b6a21798862e840a10c5754f5bce2e0e3f7d6
kunfei <gekunfei@live.com>
2020-02-18 17:35:19 +0800

    feat: 优化代码

---
+++ app/src/main/java/io/legado/app/ui/login/SourceLogin.kt
@@ -0,0 +1,14 @@
+package io.legado.app.ui.login
+
+import android.os.Bundle
+import io.legado.app.R
+import io.legado.app.base.BaseActivity
+
+
+class SourceLogin : BaseActivity(R.layout.activity_source_login) {
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+
+    }
+
+}
