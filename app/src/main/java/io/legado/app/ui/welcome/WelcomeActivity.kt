823bfe7685a3f723d4ae776e7fa958637666caae
kunfei <gekunfei@live.com>
2020-02-29 13:16:24 +0800

    feat: 优化代码

--- app/src/main/java/io/legado/app/ui/welcome/WelcomeActivity.kt
+++ app/src/main/java/io/legado/app/ui/welcome/WelcomeActivity.kt
@@ -2,14 +2,19 @@ package io.legado.app.ui.welcome
 
 import android.content.Intent
 import android.os.Bundle
+import com.github.houbb.opencc4j.util.ZhConverterUtil
+import io.legado.app.App
 import io.legado.app.R
 import io.legado.app.base.BaseActivity
+import io.legado.app.help.AppConfig
+import io.legado.app.help.coroutine.Coroutine
 import io.legado.app.lib.theme.accentColor
 import io.legado.app.ui.book.read.ReadBookActivity
 import io.legado.app.ui.main.MainActivity
 import io.legado.app.utils.getPrefBoolean
 import kotlinx.android.synthetic.main.activity_welcome.*
 import org.jetbrains.anko.startActivity
+import java.util.concurrent.TimeUnit
 
 open class WelcomeActivity : BaseActivity(R.layout.activity_welcome) {
 
@@ -19,6 +24,17 @@ open class WelcomeActivity : BaseActivity(R.layout.activity_welcome) {
         if (intent.flags and Intent.FLAG_ACTIVITY_BROUGHT_TO_FRONT != 0) {
             finish()
             return
+        }
+        Coroutine.async {
+            //清楚过期数据
+            App.db.searchBookDao()
+                .clearExpired(System.currentTimeMillis() - TimeUnit.DAYS.toMillis(1))
+            //初始化简繁转换引擎
+            when (AppConfig.chineseConverterType) {
+                1 -> ZhConverterUtil.toSimple("初始化")
+                2 -> ZhConverterUtil.toTraditional("初始化")
+                else -> null
+            }
         }
         root_view.postDelayed({
             startActivity<MainActivity>()
