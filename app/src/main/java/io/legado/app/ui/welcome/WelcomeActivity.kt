610c18e7db69a7052aba125087de8c1be8d45eac
kunfei <gekunfei@live.com>
2020-03-03 22:14:13 +0800

    feat: 优化代码

--- app/src/main/java/io/legado/app/ui/welcome/WelcomeActivity.kt
+++ app/src/main/java/io/legado/app/ui/welcome/WelcomeActivity.kt
@@ -8,6 +8,7 @@ import io.legado.app.R
 import io.legado.app.base.BaseActivity
 import io.legado.app.help.AppConfig
 import io.legado.app.help.coroutine.Coroutine
+import io.legado.app.help.storage.SyncBookProgress
 import io.legado.app.lib.theme.accentColor
 import io.legado.app.ui.book.read.ReadBookActivity
 import io.legado.app.ui.main.MainActivity
@@ -41,6 +42,7 @@ open class WelcomeActivity : BaseActivity(R.layout.activity_welcome) {
                 else -> null
             }
         }
+        SyncBookProgress.downloadBookProgress()
         root_view.postDelayed({ startMainActivity() }, 300)
     }
 
