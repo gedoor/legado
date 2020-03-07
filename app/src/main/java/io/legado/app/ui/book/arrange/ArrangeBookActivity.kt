aad32764b646396227a4a9788b69e50398ce36e4
kunfei <gekunfei@live.com>
2020-03-07 17:44:48 +0800

    feat: 优化代码

--- app/src/main/java/io/legado/app/ui/book/arrange/ArrangeBookActivity.kt
+++ app/src/main/java/io/legado/app/ui/book/arrange/ArrangeBookActivity.kt
@@ -103,7 +103,6 @@ class ArrangeBookActivity : VMBaseActivity<ArrangeBookViewModel>(R.layout.activi
                 else -> App.db.bookDao().observeByGroup(groupId)
             }
         booksLiveData?.observe(this, Observer {
-            adapter.selectedBooks.clear()
             adapter.setItems(it)
             upSelectCount()
         })
