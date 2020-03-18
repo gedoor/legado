97b644db0f82faa84204de03225f9831bce0fbe5
kunfei <gekunfei@live.com>
2020-03-18 18:08:48 +0800

    feat: 优化代码

--- app/src/main/java/io/legado/app/ui/widget/SearchView.kt
+++ app/src/main/java/io/legado/app/ui/widget/SearchView.kt
@@ -91,23 +91,23 @@ class SearchView : SearchView {
         super.setQueryHint(hint)
         updateQueryHint()
     }
-}
 
-internal class CenteredImageSpan(drawable: Drawable?) : ImageSpan(drawable!!) {
-    override fun draw(
-        canvas: Canvas, text: CharSequence,
-        start: Int, end: Int, x: Float,
-        top: Int, y: Int, bottom: Int, paint: Paint
-    ) {
-        // image to draw
-        val b = drawable
-        // font metrics of text to be replaced
-        val fm = paint.fontMetricsInt
-        val transY = ((y + fm.descent + y + fm.ascent) / 2
-                - b.bounds.bottom / 2)
-        canvas.save()
-        canvas.translate(x, transY.toFloat())
-        b.draw(canvas)
-        canvas.restore()
+    internal class CenteredImageSpan(drawable: Drawable?) : ImageSpan(drawable!!) {
+        override fun draw(
+            canvas: Canvas, text: CharSequence,
+            start: Int, end: Int, x: Float,
+            top: Int, y: Int, bottom: Int, paint: Paint
+        ) {
+            // image to draw
+            val b = drawable
+            // font metrics of text to be replaced
+            val fm = paint.fontMetricsInt
+            val transY = ((y + fm.descent + y + fm.ascent) / 2
+                    - b.bounds.bottom / 2)
+            canvas.save()
+            canvas.translate(x, transY.toFloat())
+            b.draw(canvas)
+            canvas.restore()
+        }
     }
 }
