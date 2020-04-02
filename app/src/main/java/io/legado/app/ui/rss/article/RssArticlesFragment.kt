980535d37cdd0af7351ff402a6b32eb1636fe27e
kunfei <gekunfei@live.com>
2020-04-02 14:35:14 +0800

    feat: 优化代码

--- app/src/main/java/io/legado/app/ui/rss/article/RssArticlesFragment.kt
+++ app/src/main/java/io/legado/app/ui/rss/article/RssArticlesFragment.kt
@@ -77,7 +77,7 @@ class RssArticlesFragment : VMBaseFragment<RssArticlesViewModel>(R.layout.fragme
     private fun initData() {
         activityViewModel.url?.let {
             rssArticlesData?.removeObservers(this)
-            rssArticlesData = App.db.rssArticleDao().liveByOrigin(it)
+            rssArticlesData = App.db.rssArticleDao().liveByOriginSort(it, viewModel.sortName)
             rssArticlesData?.observe(viewLifecycleOwner, Observer { list ->
                 adapter.setItems(list)
             })
