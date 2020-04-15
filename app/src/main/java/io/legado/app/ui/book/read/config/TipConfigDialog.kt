5995350bb9ab44bce1130e8e5be9f1990d40d8b5
kunfei <gekunfei@live.com>
2020-04-15 20:47:08 +0800

    feat: 优化代码

---
+++ app/src/main/java/io/legado/app/ui/book/read/config/TipConfigDialog.kt
@@ -0,0 +1,42 @@
+package io.legado.app.ui.book.read.config
+
+import android.os.Bundle
+import android.util.DisplayMetrics
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import io.legado.app.R
+import io.legado.app.base.BaseDialogFragment
+import io.legado.app.ui.book.read.Help
+
+class TipConfigDialog : BaseDialogFragment() {
+
+    override fun onStart() {
+        super.onStart()
+        val dm = DisplayMetrics()
+        activity?.let {
+            Help.upSystemUiVisibility(it)
+            it.windowManager?.defaultDisplay?.getMetrics(dm)
+        }
+        dialog?.window?.let {
+            val attr = it.attributes
+            attr.dimAmount = 0.0f
+            it.attributes = attr
+            it.setLayout((dm.widthPixels * 0.9).toInt(), ViewGroup.LayoutParams.WRAP_CONTENT)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.dialog_tip_config, container)
+    }
+
+    override fun onFragmentCreated(view: View, savedInstanceState: Bundle?) {
+
+    }
+
+
+}
