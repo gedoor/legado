e810eb9ab5e403ff80533ecd83e381ca9134c0b0
yangyxd <yangyxd@126.com>
2020-05-15 09:36:13 +0800

    Revert "替换首页图标"
    This reverts commit 1ece88762e4dffcb5224a72a77021257484d93b0.
    
    # Conflicts:
    #	app/src/main/java/io/legado/app/utils/UIUtils.kt
    #	app/src/main/res/drawable/ic_add.xml
    #	app/src/main/res/drawable/ic_add_online.xml
    #	app/src/main/res/drawable/ic_arrange.xml
    #	app/src/main/res/drawable/ic_download_line.xml
    #	app/src/main/res/drawable/ic_groups.xml
    #	app/src/main/res/drawable/ic_search.xml
    #	app/src/main/res/drawable/ic_view_quilt.xml

--- app/src/main/java/io/legado/app/ui/main/MainActivity.kt
+++ app/src/main/java/io/legado/app/ui/main/MainActivity.kt
@@ -15,7 +15,6 @@ import io.legado.app.base.VMBaseActivity
 import io.legado.app.constant.EventBus
 import io.legado.app.constant.PreferKey
 import io.legado.app.help.AppConfig
-import io.legado.app.help.storage.Backup
 import io.legado.app.lib.theme.ATH
 import io.legado.app.service.BaseReadAloudService
 import io.legado.app.service.help.ReadAloud
@@ -78,7 +77,7 @@ class MainActivity : VMBaseActivity<MainViewModel>(R.layout.activity_main),
             putPrefInt(PreferKey.versionCode, App.INSTANCE.versionCode)
             if (!BuildConfig.DEBUG) {
                 val log = String(assets.open("updateLog.md").readBytes())
-                TextDialog.show(supportFragmentManager, log, TextDialog.MD, 5000, true)
+                TextDialog.show(supportFragmentManager, log, TextDialog.MD, 5000)
             }
         }
     }
@@ -112,13 +111,6 @@ class MainActivity : VMBaseActivity<MainViewModel>(R.layout.activity_main),
             }
         }
         return super.onKeyUp(keyCode, event)
-    }
-
-    override fun onPause() {
-        super.onPause()
-        if (!BuildConfig.DEBUG) {
-            Backup.autoBack(this)
-        }
     }
 
     override fun onDestroy() {
