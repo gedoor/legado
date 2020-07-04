e58a80156d284525c6fb4fb51cf6d7aab06e9534
gedoor <gekunfei@live.com>
2020-07-04 17:52:26 +0800

    feat: 添加6群群号

--- app/src/main/java/io/legado/app/ui/about/AboutFragment.kt
+++ app/src/main/java/io/legado/app/ui/about/AboutFragment.kt
@@ -27,7 +27,8 @@ class AboutFragment : PreferenceFragmentCompat() {
         Pair("(QQ群2)773736122", "5Bm5w6OgLupXnICbYvbgzpPUgf0UlsJF"),
         Pair("(QQ群3)981838750", "g_Sgmp2nQPKqcZQ5qPcKLHziwX_mpps9"),
         Pair("(QQ群4)256929088", "czEJPLDnT4Pd9SKQ6RoRVzKhDxLchZrO"),
-        Pair("(QQ群5)811843556", "zKZ2UYGZ7o5CzcA6ylxzlqi21si_iqaX")
+        Pair("(QQ群5)811843556", "zKZ2UYGZ7o5CzcA6ylxzlqi21si_iqaX"),
+        Pair("(QQ群6)870270970", "FeCF8iSxfQbe90HPvGsvcqs5P5oSeY5n")
     )
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
