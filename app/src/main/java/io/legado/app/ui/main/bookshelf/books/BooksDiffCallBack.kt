a947fd60be1a14b494248cba04404a65779adf2f
gedoor <gekunfei@live.com>
2020-08-03 17:24:24 +0800

    优化

--- app/src/main/java/io/legado/app/ui/main/bookshelf/books/BooksDiffCallBack.kt
+++ app/src/main/java/io/legado/app/ui/main/bookshelf/books/BooksDiffCallBack.kt
@@ -42,20 +42,21 @@ class BooksDiffCallBack(private val oldItems: List<Book>, private val newItems: 
         val oldItem = oldItems[oldItemPosition]
         val newItem = newItems[newItemPosition]
         val bundle = bundleOf()
-        if (oldItem.name != newItem.name) bundle.putString("name", newItem.name)
-        if (oldItem.author != newItem.author) bundle.putString("author", newItem.author)
-        if (oldItem.durChapterTitle != newItem.durChapterTitle) bundle.putString(
-            "dur",
-            newItem.durChapterTitle
-        )
-        if (oldItem.latestChapterTitle != newItem.latestChapterTitle) bundle.putString(
-            "last",
-            newItem.latestChapterTitle
-        )
-        if (oldItem.getDisplayCover() != newItem.getDisplayCover()) bundle.putString(
-            "cover",
-            newItem.getDisplayCover()
-        )
+        if (oldItem.name != newItem.name) {
+            bundle.putString("name", newItem.name)
+        }
+        if (oldItem.author != newItem.author) {
+            bundle.putString("author", newItem.author)
+        }
+        if (oldItem.durChapterTitle != newItem.durChapterTitle) {
+            bundle.putString("dur", newItem.durChapterTitle)
+        }
+        if (oldItem.latestChapterTitle != newItem.latestChapterTitle) {
+            bundle.putString("last", newItem.latestChapterTitle)
+        }
+        if (oldItem.getDisplayCover() != newItem.getDisplayCover()) {
+            bundle.putString("cover", newItem.getDisplayCover())
+        }
         if (oldItem.lastCheckCount != newItem.lastCheckCount
             || oldItem.durChapterTime != newItem.durChapterTime
             || oldItem.getUnreadChapterNum() != newItem.getUnreadChapterNum()
