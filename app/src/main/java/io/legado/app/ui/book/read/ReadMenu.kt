98e18f879f1f41fbe70ef585f95445717cdc17ea
gedoor <gekunfei@live.com>
2020-08-09 15:55:58 +0800

    给亮度调节加个背景,很多人找不到

--- app/src/main/java/io/legado/app/ui/book/read/ReadMenu.kt
+++ app/src/main/java/io/legado/app/ui/book/read/ReadMenu.kt
@@ -13,10 +13,7 @@ import io.legado.app.R
 import io.legado.app.constant.EventBus
 import io.legado.app.help.AppConfig
 import io.legado.app.help.ReadBookConfig
-import io.legado.app.lib.theme.Selector
-import io.legado.app.lib.theme.accentColor
-import io.legado.app.lib.theme.bottomBackground
-import io.legado.app.lib.theme.buttonDisabledColor
+import io.legado.app.lib.theme.*
 import io.legado.app.service.help.ReadBook
 import io.legado.app.utils.*
 import kotlinx.android.synthetic.main.view_read_menu.view.*
@@ -56,6 +53,7 @@ class ReadMenu : FrameLayout {
             fabNightTheme.setImageResource(R.drawable.ic_brightness)
         }
         initAnimation()
+        ll_brightness.background = ATH.getDialogBackground()
         ll_bottom_bg.setBackgroundColor(context.bottomBackground)
         vw_bg.onClick { }
         vwNavigationBar.onClick { }
