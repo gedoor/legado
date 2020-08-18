deda8c40a5fc1508c64a367f7e847bf9f114f57d
AndyBernie <zhenghaotian2016@gmail.com>
2020-08-18 09:22:51 +0800

    Update ImageProvider.kt
    加入baseUrl变量申明

--- app/src/main/java/io/legado/app/ui/book/read/page/provider/ImageProvider.kt
+++ app/src/main/java/io/legado/app/ui/book/read/page/provider/ImageProvider.kt
@@ -31,7 +31,7 @@ object ImageProvider {
         indexCache[src] = bitmap
     }
 
-    fun getImage(book: Book, chapterIndex: Int, src: String, onUi: Boolean = false): Bitmap? {
+    fun getImage(book: Book, chapterIndex: Int, src: String, baseUrl: String, onUi: Boolean = false): Bitmap? {
         getCache(chapterIndex, src)?.let {
             return it
         }
@@ -49,7 +49,7 @@ object ImageProvider {
                     out.close()
                 }
             } else if (!onUi) {
-                HttpHelper.getBytes(src, src)?.let {
+                HttpHelper.getBytes(src, baseUrl)?.let {
                     FileUtils.createFileIfNotExist(vFile.absolutePath).writeBytes(it)
                 }
             }
