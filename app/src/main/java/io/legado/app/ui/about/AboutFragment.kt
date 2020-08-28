42830f372978cc75daf0bc7e19ae884542aec2db
gedoor <gekunfei@live.com>
2020-08-28 17:44:49 +0800

    优化主题色

--- app/src/main/java/io/legado/app/ui/about/AboutFragment.kt
+++ app/src/main/java/io/legado/app/ui/about/AboutFragment.kt
@@ -11,6 +11,7 @@ import io.legado.app.App
 import io.legado.app.R
 import io.legado.app.lib.dialogs.alert
 import io.legado.app.ui.widget.dialog.TextDialog
+import io.legado.app.utils.applyTint
 import io.legado.app.utils.openUrl
 import io.legado.app.utils.sendToClip
 import io.legado.app.utils.toast
@@ -92,7 +93,7 @@ class AboutFragment : PreferenceFragmentCompat() {
                     }
                 }
             }
-        }.show()
+        }.show().applyTint()
     }
 
     private fun joinQQGroup(key: String): Boolean {
