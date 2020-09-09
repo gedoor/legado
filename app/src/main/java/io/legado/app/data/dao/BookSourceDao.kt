609fb92aee905257ebe429643d409e73f9b8a7da
Jason Yao <h11129@gmail.com>
2020-09-09 16:40:41 -0400

    updatetime 和 comment 优化
    当书源有改动时更新书源的lastupdatetime
    改变booksourcecomment位置到booksourcegroup之下

--- app/src/main/java/io/legado/app/data/dao/BookSourceDao.kt
+++ app/src/main/java/io/legado/app/data/dao/BookSourceDao.kt
@@ -11,7 +11,7 @@ interface BookSourceDao {
     @Query("select * from book_sources order by customOrder asc")
     fun liveDataAll(): LiveData<List<BookSource>>
 
-    @Query("select * from book_sources where bookSourceName like :searchKey or bookSourceGroup like :searchKey or bookSourceUrl like :searchKey order by customOrder asc")
+    @Query("select * from book_sources where bookSourceName like :searchKey or bookSourceGroup like :searchKey or bookSourceUrl like :searchKey  or bookSourceComment like :searchKey order by customOrder asc")
     fun liveDataSearch(searchKey: String = ""): LiveData<List<BookSource>>
 
     @Query("select * from book_sources where enabled = 1 order by customOrder asc")
