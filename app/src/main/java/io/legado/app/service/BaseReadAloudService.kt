45e88d5ade10c30e82adf5c105c28f3d95463c83
口口吕 <156081428@qq.com>
2020-10-03 17:57:02 +0800

    优化朗读逻辑

--- app/src/main/java/io/legado/app/service/BaseReadAloudService.kt
+++ app/src/main/java/io/legado/app/service/BaseReadAloudService.kt
@@ -263,17 +263,19 @@ abstract class BaseReadAloudService : BaseService(),
         when (focusChange) {
             AudioManager.AUDIOFOCUS_GAIN -> {
                 // 重新获得焦点,  可做恢复播放，恢复后台音量的操作
-                if (!pause) resumeReadAloud()
+                if (pause) resumeReadAloud()
             }
             AudioManager.AUDIOFOCUS_LOSS -> {
                 // 永久丢失焦点除非重新主动获取，这种情况是被其他播放器抢去了焦点，  为避免与其他播放器混音，可将音乐暂停
+                if (!pause) pauseReadAloud(pause = true)
             }
             AudioManager.AUDIOFOCUS_LOSS_TRANSIENT -> {
                 // 暂时丢失焦点，这种情况是被其他应用申请了短暂的焦点，可压低后台音量
-                if (!pause) pauseReadAloud(false)
+                if (!pause) pauseReadAloud(pause = true)
             }
             AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK -> {
                 // 短暂丢失焦点，这种情况是被其他应用申请了短暂的焦点希望其他声音能压低音量（或者关闭声音）凸显这个声音（比如短信提示音），
+                if (!pause) pauseReadAloud(pause = true)
             }
         }
     }
