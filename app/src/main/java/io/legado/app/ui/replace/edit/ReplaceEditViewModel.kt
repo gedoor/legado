8ce3ff401a2fe9194691bac4d369124fa2a5a344
gedoor <gekunfei@live.com>
2020-11-05 22:23:52 +0800

    替换界面添加正则帮助链接

--- app/src/main/java/io/legado/app/ui/replace/edit/ReplaceEditViewModel.kt
+++ app/src/main/java/io/legado/app/ui/replace/edit/ReplaceEditViewModel.kt
@@ -2,35 +2,36 @@ package io.legado.app.ui.replace.edit
 
 import android.app.Application
 import android.content.Intent
-import androidx.lifecycle.MutableLiveData
 import io.legado.app.App
 import io.legado.app.base.BaseViewModel
 import io.legado.app.data.entities.ReplaceRule
 
 class ReplaceEditViewModel(application: Application) : BaseViewModel(application) {
 
-    val replaceRuleData = MutableLiveData<ReplaceRule>()
+    var replaceRule: ReplaceRule? = null
 
-    fun initData(intent: Intent) {
+    fun initData(intent: Intent, finally: (replaceRule: ReplaceRule) -> Unit) {
         execute {
-            replaceRuleData.value ?: let {
-                val id = intent.getLongExtra("id", -1)
-                if (id > 0) {
-                    App.db.replaceRuleDao().findById(id)?.let {
-                        replaceRuleData.postValue(it)
-                    }
-                } else {
-                    val pattern = intent.getStringExtra("pattern") ?: ""
-                    val isRegex = intent.getBooleanExtra("isRegex", false)
-                    val scope = intent.getStringExtra("scope")
-                    val rule = ReplaceRule(
-                        name = pattern,
-                        pattern = pattern,
-                        isRegex = isRegex,
-                        scope = scope
-                    )
-                    replaceRuleData.postValue(rule)
+            val id = intent.getLongExtra("id", -1)
+            if (id > 0) {
+                App.db.replaceRuleDao().findById(id)?.let {
+                    replaceRule = it
                 }
+            } else {
+                val pattern = intent.getStringExtra("pattern") ?: ""
+                val isRegex = intent.getBooleanExtra("isRegex", false)
+                val scope = intent.getStringExtra("scope")
+                val rule = ReplaceRule(
+                    name = pattern,
+                    pattern = pattern,
+                    isRegex = isRegex,
+                    scope = scope
+                )
+                replaceRule = rule
+            }
+        }.onFinally {
+            replaceRule?.let {
+                finally(it)
             }
         }
     }
