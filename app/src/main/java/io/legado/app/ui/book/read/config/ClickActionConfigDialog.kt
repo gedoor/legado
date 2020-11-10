792dd9497b3f4b28d68cd78669e0b0b2dfb9831d
gedoor <gekunfei@live.com>
2020-11-10 23:58:02 +0800

    增强点击翻页设置

--- app/src/main/java/io/legado/app/ui/book/read/config/ClickActionConfigDialog.kt
+++ app/src/main/java/io/legado/app/ui/book/read/config/ClickActionConfigDialog.kt
@@ -72,41 +72,49 @@ class ClickActionConfigDialog : BaseDialogFragment() {
         tv_top_left.onClick {
             selectAction { action ->
                 putPrefInt(PreferKey.clickActionTopLeft, action)
+                initData()
             }
         }
         tv_top_center.onClick {
             selectAction { action ->
                 putPrefInt(PreferKey.clickActionTopCenter, action)
+                initData()
             }
         }
         tv_top_right.onClick {
             selectAction { action ->
                 putPrefInt(PreferKey.clickActionTopRight, action)
+                initData()
             }
         }
         tv_middle_left.onClick {
             selectAction { action ->
                 putPrefInt(PreferKey.clickActionMiddleLeft, action)
+                initData()
             }
         }
         tv_middle_right.onClick {
             selectAction { action ->
                 putPrefInt(PreferKey.clickActionMiddleRight, action)
+                initData()
             }
         }
         tv_bottom_left.onClick {
             selectAction { action ->
                 putPrefInt(PreferKey.clickActionBottomLeft, action)
+                initData()
             }
         }
         tv_bottom_center.onClick {
             selectAction { action ->
                 putPrefInt(PreferKey.clickActionBottomCenter, action)
+                initData()
             }
         }
         tv_bottom_right.onClick {
             selectAction { action ->
                 putPrefInt(PreferKey.clickActionBottomRight, action)
+                initData()
             }
         }
     }
