0b6249b9a714799f2baffcad03c8f23ca4be0dc4
gedoor <gekunfei@live.com>
2020-11-10 21:46:06 +0800

    优化

--- app/src/main/java/io/legado/app/ui/book/read/config/ClickActionConfigDialog.kt
+++ app/src/main/java/io/legado/app/ui/book/read/config/ClickActionConfigDialog.kt
@@ -1,11 +1,13 @@
 package io.legado.app.ui.book.read.config
 
+import android.content.DialogInterface
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import io.legado.app.R
 import io.legado.app.base.BaseDialogFragment
+import io.legado.app.ui.book.read.ReadBookActivity
 import io.legado.app.utils.getCompatColor
 import kotlinx.android.synthetic.main.dialog_click_action_config.*
 import org.jetbrains.anko.sdk27.listeners.onClick
@@ -14,6 +16,7 @@ class ClickActionConfigDialog : BaseDialogFragment() {
 
     override fun onStart() {
         super.onStart()
+        (activity as ReadBookActivity).bottomDialog++
         dialog?.window?.let {
             it.setBackgroundDrawableResource(R.color.transparent)
             it.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
@@ -26,6 +29,11 @@ class ClickActionConfigDialog : BaseDialogFragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.dialog_click_action_config, container)
+    }
+
+    override fun onDismiss(dialog: DialogInterface) {
+        super.onDismiss(dialog)
+        (activity as ReadBookActivity).bottomDialog--
     }
 
     override fun onFragmentCreated(view: View, savedInstanceState: Bundle?) {
