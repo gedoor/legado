755bc626324719961bce29a5e11ebab4b4a539fe
gedoor <gekunfei@live.com>
2020-12-03 19:28:21 +0800

    优化

--- app/src/main/java/io/legado/app/help/BookHelp.kt
+++ app/src/main/java/io/legado/app/help/BookHelp.kt
@@ -225,7 +225,8 @@ object BookHelp {
         oldDurChapterName: String?,
         newChapterList: List<BookChapter>
     ): Int {
-        if (oldChapterListSize == 0) return 0
+        if (oldChapterListSize == 0) return oldDurChapterIndex
+        if (newChapterList.isEmpty()) return oldDurChapterIndex
         val oldChapterNum = getChapterNum(oldDurChapterName)
         val oldName = getPureChapterName(oldDurChapterName)
         val newChapterSize = newChapterList.size
