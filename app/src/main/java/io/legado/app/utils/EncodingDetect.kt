54f23b9ed89e7fbf561dae1506f7970c98c1d2a3
gedoor <gekunfei@live.com>
2020-12-09 10:45:39 +0800

    优化编码识别

--- app/src/main/java/io/legado/app/utils/EncodingDetect.kt
+++ app/src/main/java/io/legado/app/utils/EncodingDetect.kt
@@ -9,28 +9,8 @@ import java.nio.charset.StandardCharsets
 import java.util.*
 
 /**
- * <Detect encoding .> Copyright (C) <2009> <Fluck></Fluck>,ACC http://androidos.cc/dev>
- *
- *
- * This program is free software: you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option) any later
- * version.
- *
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
- * details.
- *
- *
- * EncodingDetect.java<br></br>
  * 自动获取文件的编码
- *
- * @author Billows.Van
- * @version 1.0
- * @since Create on 2010-01-27 11:19:00
-</Detect> */
+ * */
 @Suppress("MemberVisibilityCanBePrivate", "unused")
 object EncodingDetect {
 
