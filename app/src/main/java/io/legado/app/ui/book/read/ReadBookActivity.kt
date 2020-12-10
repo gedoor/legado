445ededd33fc7c4bb57e55e6c270cf99f3d0a13e
gedoor <gekunfei@live.com>
2020-12-10 20:37:57 +0800

    优化

--- app/src/main/java/io/legado/app/ui/book/read/ReadBookActivity.kt
+++ app/src/main/java/io/legado/app/ui/book/read/ReadBookActivity.kt
@@ -173,7 +173,6 @@ class ReadBookActivity : ReadBookBaseActivity(),
                     else -> when (item.itemId) {
                         R.id.menu_enable_replace -> item.isChecked = book.getUseReplaceRule()
                         R.id.menu_re_segment -> item.isChecked = book.getReSegment()
-                        R.id.menu_get_progress -> BookWebDav.initWebDav()
                     }
                 }
             }
