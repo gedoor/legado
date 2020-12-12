dad0679a74bc28739b9b3465aa7b32f782a180c1
gedoor <gekunfei@live.com>
2020-12-12 21:31:04 +0800

    换源添加分组选择

--- app/src/main/java/io/legado/app/data/dao/SearchBookDao.kt
+++ app/src/main/java/io/legado/app/data/dao/SearchBookDao.kt
@@ -27,22 +27,27 @@ interface SearchBookDao {
         select t1.name, t1.author, t1.origin, t1.originName, t1.coverUrl, t1.bookUrl, t1.type, t1.time, t1.intro, t1.kind, t1.latestChapterTitle, t1.tocUrl, t1.variable, t1.wordCount, t2.customOrder as originOrder
         from searchBooks as t1 inner join book_sources as t2 
         on t1.origin = t2.bookSourceUrl 
-        where t1.name = :name and t1.author = :author and t2.enabled = 1
+        where t1.name = :name and t1.author = :author and t2.enabled = 1 and t2.bookSourceGroup like '%'||:sourceGroup||'%'
         order by t2.customOrder
         """
     )
-    fun getByNameAuthorEnable(name: String, author: String): List<SearchBook>
+    fun getChangeSourceSearch(name: String, author: String, sourceGroup: String): List<SearchBook>
 
     @Query(
         """
         select t1.name, t1.author, t1.origin, t1.originName, t1.coverUrl, t1.bookUrl, t1.type, t1.time, t1.intro, t1.kind, t1.latestChapterTitle, t1.tocUrl, t1.variable, t1.wordCount, t2.customOrder as originOrder
         from searchBooks as t1 inner join book_sources as t2 
         on t1.origin = t2.bookSourceUrl 
-        where t1.name = :name and t1.author = :author and originName like '%'||:key||'%' and t2.enabled = 1
+        where t1.name = :name and t1.author = :author and originName like '%'||:key||'%' and t2.enabled = 1 and t2.bookSourceGroup like '%'||:sourceGroup||'%'
         order by t2.customOrder
         """
     )
-    fun getChangeSourceSearch(name: String, author: String, key: String): List<SearchBook>
+    fun getChangeSourceSearch(
+        name: String,
+        author: String,
+        key: String,
+        sourceGroup: String
+    ): List<SearchBook>
 
     @Query(
         """
