de19f42487bc80d95a1f986f00e1e152f19517ff
gedoor <gekunfei@live.com>
2021-01-12 18:05:43 +0800

    优化

--- app/src/main/java/io/legado/app/ui/book/changesource/ChangeSourceViewModel.kt
+++ app/src/main/java/io/legado/app/ui/book/changesource/ChangeSourceViewModel.kt
@@ -100,6 +100,8 @@ class ChangeSourceViewModel(application: Application) : BaseViewModel(applicatio
     private fun startSearch() {
         execute {
             App.db.searchBookDao.clear(name, author)
+            searchBooks.clear()
+            upAdapter()
             bookSourceList.clear()
             if (searchGroup.isBlank()) {
                 bookSourceList.addAll(App.db.bookSourceDao.allEnabled)
