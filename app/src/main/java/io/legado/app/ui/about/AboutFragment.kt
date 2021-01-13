883f3f7e39b92728c1cc63210abeaff4994c67e9
gedoor <gekunfei@live.com>
2021-01-13 13:35:22 +0800

    优化

--- app/src/main/java/io/legado/app/ui/about/AboutFragment.kt
+++ app/src/main/java/io/legado/app/ui/about/AboutFragment.kt
@@ -24,6 +24,7 @@ class AboutFragment : PreferenceFragmentCompat() {
         Pair("(QQ群VIP1)701903217", "-iolizL4cbJSutKRpeImHlXlpLDZnzeF"),
         Pair("(QQ群VIP2)263949160", "xwfh7_csb2Gf3Aw2qexEcEtviLfLfd4L"),
         Pair("(QQ群VIP3)680280282", "_N0i7yZObjKSeZQvzoe2ej7j02kLnOOK"),
+        Pair("(QQ群VIP4)680280282", "VF2UwvUCuaqlo6pddWTe_kw__a1_Fr8O"),
         Pair("(QQ群1)805192012", "6GlFKjLeIk5RhQnR3PNVDaKB6j10royo"),
         Pair("(QQ群2)773736122", "5Bm5w6OgLupXnICbYvbgzpPUgf0UlsJF"),
         Pair("(QQ群3)981838750", "g_Sgmp2nQPKqcZQ5qPcKLHziwX_mpps9"),
