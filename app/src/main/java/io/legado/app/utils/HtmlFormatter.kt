c24eb1b807b5a377bc5882fe7f85f0699169be44
bushixuanqi <57338301+bushixuanqi@users.noreply.github.com>
2021-06-13 20:16:59 +0800

    Update HtmlFormatter.kt
    简化网页格式化程序，并修复格式化图片只格式了第一张的问题。

--- app/src/main/java/io/legado/app/utils/HtmlFormatter.kt
+++ app/src/main/java/io/legado/app/utils/HtmlFormatter.kt
@@ -2,55 +2,49 @@ package io.legado.app.utils
 
 import io.legado.app.model.analyzeRule.AnalyzeUrl
 import java.net.URL
-import java.util.regex.Pattern
+import io.legado.app.constant.AppPattern
 
 object HtmlFormatter {
     private val wrapHtmlRegex = "</?(?:div|p|br|hr|h\\d|article|dd|dl)[^>]*>".toRegex()
     private val notImgHtmlRegex = "</?(?!img)[a-zA-Z]+(?=[ >])[^<>]*>".toRegex()
     private val otherHtmlRegex = "</?[a-zA-Z]+(?=[ >])[^<>]*>".toRegex()
-    private val imgPattern = Pattern.compile("<img [^>]*src=.*?\"(.*?(?:,\\{.*\\})?)\".*?>")
 
-    fun format(html: String?): String {
+    fun format(html: String?, otherRegex: Regex = otherHtmlRegex): String {
         html ?: return ""
         return html.replace(wrapHtmlRegex, "\n")
-            .replace(otherHtmlRegex, "")
-            .replace("\\s*\\n+\\s*".toRegex(), "\n　　")
+            .replace(otherRegex, "")
             .replace("^[\\n\\s]+".toRegex(), "　　")
             .replace("[\\n\\s]+$".toRegex(), "")
+            .replace("\\s*\\n+\\s*".toRegex(), "\n　　")
     }
 
-    fun formatKeepImg(html: String?): String {
-        html ?: return ""
-        return html.replace(wrapHtmlRegex, "\n")
-            .replace(notImgHtmlRegex, "")
-            .replace("\\s*\\n+\\s*".toRegex(), "\n　　")
-            .replace("^[\\n\\s]+".toRegex(), "　　")
-            .replace("[\\n\\s]+$".toRegex(), "")
-    }
+    fun formatKeepImg(html: String?) = format(html,notImgHtmlRegex)
 
     fun formatKeepImg(html: String?, redirectUrl: URL?): String {
         html ?: return ""
+        var formatHtml = formatKeepImg(html)
         val sb = StringBuffer()
-        val matcher = imgPattern.matcher(html)
         var appendPos = 0
-        while (matcher.find()) {
-            val urlArray = matcher.group(1)!!.split(AnalyzeUrl.splitUrlRegex)
-            var url = NetworkUtils.getAbsoluteURL(redirectUrl, urlArray[0])
-            if (urlArray.size > 1) {
-                url = "$url,${urlArray[1]}"
+        while (appendPos < formatHtml.length) {
+            val matcher = AppPattern.imgPattern.matcher(formatHtml)
+            if(matcher.find()) {
+                val urlArray = matcher.group(1)!!.split(AnalyzeUrl.splitUrlRegex)
+                var url = NetworkUtils.getAbsoluteURL(redirectUrl, urlArray[0])
+                if (urlArray.size > 1) {
+                    url = "$url,${urlArray[1]}"
+                }
+                sb.append(formatHtml.substring(appendPos, matcher.start()))
+                sb.append("<img src=\"$url\" >")
+                appendPos = matcher.end()
+                formatHtml = formatHtml.substring(appendPos, formatHtml.length)
+                appendPos = 0
+            } else {
+                sb.append(formatHtml)
+                appendPos = formatHtml.length
             }
-            sb.append(html.substring(appendPos, matcher.start()))
-            sb.append("<img src=\"$url\" >")
-            appendPos = matcher.end()
         }
-        if (appendPos < html.length) {
-            sb.append(html.substring(appendPos, html.length))
-        }
-        return sb.replace(wrapHtmlRegex, "\n")
-            .replace(notImgHtmlRegex, "")
-            .replace("\\s*\\n+\\s*".toRegex(), "\n　　")
-            .replace("^[\\n\\s]+".toRegex(), "　　")
-            .replace("[\\n\\s]+$".toRegex(), "")
+
+        return sb.toString()
     }
 
 }
