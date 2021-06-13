509709dc6667b284ebc09d07113d6142707e5006
bushixuanqi <57338301+bushixuanqi@users.noreply.github.com>
2021-06-13 20:03:04 +0800

    Update AppPattern.kt
    优化图片正则、书名正则、作者名正则
    
    图片有data-开头的数据属性时优先用数据属性作为src，没有数据属性时才匹配原生src属性

--- app/src/main/java/io/legado/app/constant/AppPattern.kt
+++ app/src/main/java/io/legado/app/constant/AppPattern.kt
@@ -7,11 +7,12 @@ object AppPattern {
     val JS_PATTERN: Pattern =
         Pattern.compile("(<js>[\\w\\W]*?</js>|@js:[\\w\\W]*$)", Pattern.CASE_INSENSITIVE)
     val EXP_PATTERN: Pattern = Pattern.compile("\\{\\{([\\w\\W]*?)\\}\\}")
+    //图片有data-开头的数据属性时优先用数据属性作为src，没有数据属性时才匹配src
     val imgPattern: Pattern =
-        Pattern.compile("<img .*?src.*?=.*?\"(.*?(?:,\\{.*\\})?)\".*?>", Pattern.CASE_INSENSITIVE)
+        Pattern.compile("<img(?:(?![^>]*data-)src|(?=[^>]*data-)[^>]*data-[^= ]+) *= *\"([^\"{]+(?:\\{(?:[^{}]|\\{[^{}]*\\})*\\})?)\"[^>]*>", Pattern.CASE_INSENSITIVE)
 
-    val nameRegex = Regex("\\s+作\\s*者.*")
-    val authorRegex = Regex(".*?作\\s*?者[:：]")
+    val nameRegex = Regex("(?<=\\S)\\s+作\\s*者.*|\\s+\\S+\\s+著")
+    val authorRegex = Regex("^.*?作\\s*者[:：\\s]*|\\s+著")
     val fileNameRegex = Regex("[\\\\/:*?\"<>|.]")
     val splitGroupRegex = Regex("[,;，；]")
 }
