07dcc01842a126d7187dadfe0ca86fa57d40a7d9
bushixuanqi <2208877960@qq.com>
2021-07-17 02:15:39 +0800

    优化链接分割规则，避免附加参数中存在 ,{ 时导致规则切错
    修复<str0,str2,...{{js}}>这种页数列表写法中，js部分内含 < 或 > 会切割错误的问题
    
    格式化图片和匹配图片规则不在使用同一个正则，加快图片处理速度。
    
    优化一下不需要循环执行却被循环执行的操作

--- app/src/main/java/io/legado/app/constant/AppPattern.kt
+++ app/src/main/java/io/legado/app/constant/AppPattern.kt
@@ -5,11 +5,11 @@ import java.util.regex.Pattern
 @Suppress("RegExpRedundantEscape")
 object AppPattern {
     val JS_PATTERN: Pattern =
-        Pattern.compile("(<js>[\\w\\W]*?</js>|@js:[\\w\\W]*$)", Pattern.CASE_INSENSITIVE)
+        Pattern.compile("<js>([\\w\\W]+?)</js>|@js:([\\w\\W]*)", Pattern.CASE_INSENSITIVE)
     val EXP_PATTERN: Pattern = Pattern.compile("\\{\\{([\\w\\W]*?)\\}\\}")
-    //图片有data-开头的数据属性时优先用数据属性作为src，没有数据属性时才匹配src
-    val imgPattern: Pattern =
-        Pattern.compile("<img(?:(?![^>]*data-)[^>]*src|[^>]*data-)[^=]*= *\"([^\"{]+(?:\\{(?:[^{}]|\\{[^{}]*\\})*\\})?)\"[^>]*>", Pattern.CASE_INSENSITIVE)
+
+    //匹配格式化后的图片格式
+    val imgPattern: Pattern = Pattern.compile("<img src=\"([^>]+)\">")
 
     val nameRegex = Regex("\\s+作\\s*者.*|\\s+\\S+\\s+著")
     val authorRegex = Regex("^.*?作\\s*者[:：\\s]*|\\s+著")
