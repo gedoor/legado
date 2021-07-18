f9324fee5f80398950e5ee5b81fc3ef0159d5267
bushixuanqi <57338301+bushixuanqi@users.noreply.github.com>
2021-07-18 13:46:32 +0800

    Update HtmlFormatter.kt
    优化

--- app/src/main/java/io/legado/app/utils/HtmlFormatter.kt
+++ app/src/main/java/io/legado/app/utils/HtmlFormatter.kt
@@ -36,7 +36,7 @@ object HtmlFormatter {
                 val urlBefore = url.substringBefore(',')
                 val strBefore = keepImgHtml.substring(appendPos, matcher.start())
                 sb.append(
-                    if(strBefore.isBlank()) strBefore else strBefore.replace("\n", "\n　　") //缩进图片之间的非空白段落
+                    if(strBefore.isBlank()) '\n' else strBefore.replace("\n", "\n　　") //缩进图片之间的非空白段落
                 )
                 sb.append(
                     "<img src=\"${
