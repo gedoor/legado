7cdf7b602d9387581fd7f1447043c3b089a2a1aa
bushixuanqi <57338301+bushixuanqi@users.noreply.github.com>
2021-07-18 20:24:46 +0800

    Update HtmlFormatter.kt
    优化

--- app/src/main/java/io/legado/app/utils/HtmlFormatter.kt
+++ app/src/main/java/io/legado/app/utils/HtmlFormatter.kt
@@ -24,7 +24,7 @@ object HtmlFormatter {
         html ?: return ""
         val keepImgHtml = html.replace(wrapHtmlRegex, "\n")
             .replace(notImgHtmlRegex, "")
-            .replace("\\n\\s*\\n|\\n$|^\\n".toRegex(), "\n")
+            .replace("\\n\\s*\\n|\\n\\s*$|^\\s*\\n".toRegex(), "\n")
 
         var str = StringBuffer()
         var endPos = 0
