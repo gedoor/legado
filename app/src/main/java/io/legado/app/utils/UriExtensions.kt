c43dcd121628d05e3ee64ce5d459c53657551f81
gedoor <gekunfei@live.com>
2021-08-12 22:56:42 +0800

    优化

--- app/src/main/java/io/legado/app/utils/UriExtensions.kt
+++ app/src/main/java/io/legado/app/utils/UriExtensions.kt
@@ -2,10 +2,67 @@ package io.legado.app.utils
 
 import android.content.Context
 import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
 import androidx.documentfile.provider.DocumentFile
+import androidx.fragment.app.Fragment
+import io.legado.app.R
+import io.legado.app.lib.permission.Permissions
+import io.legado.app.lib.permission.PermissionsCompat
 import java.io.File
 
 fun Uri.isContentScheme() = this.scheme == "content"
+
+@Throws(Exception::class)
+fun Uri.read(activity: AppCompatActivity, success: (name: String, bytes: ByteArray) -> Unit) {
+    if (isContentScheme()) {
+        val doc = DocumentFile.fromSingleUri(activity, this)
+        doc ?: error("未获取到文件")
+        val name = doc.name ?: error("未获取到文件名")
+        val fileBytes = DocumentUtils.readBytes(activity, doc.uri)
+        fileBytes ?: error("读取文件出错")
+        success.invoke(name, fileBytes)
+    } else {
+        PermissionsCompat.Builder(activity)
+            .addPermissions(
+                Permissions.READ_EXTERNAL_STORAGE,
+                Permissions.WRITE_EXTERNAL_STORAGE
+            )
+            .rationale(R.string.bg_image_per)
+            .onGranted {
+                RealPathUtil.getPath(activity, this)?.let { path ->
+                    val imgFile = File(path)
+                    success.invoke(imgFile.name, imgFile.readBytes())
+                }
+            }
+            .request()
+    }
+}
+
+@Throws(Exception::class)
+fun Uri.read(fragment: Fragment, success: (name: String, bytes: ByteArray) -> Unit) {
+    if (isContentScheme()) {
+        val doc = DocumentFile.fromSingleUri(fragment.requireContext(), this)
+        doc ?: error("未获取到文件")
+        val name = doc.name ?: error("未获取到文件名")
+        val fileBytes = DocumentUtils.readBytes(fragment.requireContext(), doc.uri)
+        fileBytes ?: error("读取文件出错")
+        success.invoke(name, fileBytes)
+    } else {
+        PermissionsCompat.Builder(fragment)
+            .addPermissions(
+                Permissions.READ_EXTERNAL_STORAGE,
+                Permissions.WRITE_EXTERNAL_STORAGE
+            )
+            .rationale(R.string.bg_image_per)
+            .onGranted {
+                RealPathUtil.getPath(fragment.requireContext(), this)?.let { path ->
+                    val imgFile = File(path)
+                    success.invoke(imgFile.name, imgFile.readBytes())
+                }
+            }
+            .request()
+    }
+}
 
 @Throws(Exception::class)
 fun Uri.readBytes(context: Context): ByteArray? {
