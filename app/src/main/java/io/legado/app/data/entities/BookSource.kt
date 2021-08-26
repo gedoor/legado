7e469d144b1e3336f59ef52189c29b137b5e60ea
gedoor <gekunfei@live.com>
2021-08-26 13:46:27 +0800

    优化

--- app/src/main/java/io/legado/app/data/entities/BookSource.kt
+++ app/src/main/java/io/legado/app/data/entities/BookSource.kt
@@ -28,7 +28,7 @@ data class BookSource(
     var enabled: Boolean = true,                    // 是否启用
     var enabledExplore: Boolean = true,             // 启用发现
     override var header: String? = null,            // 请求头
-    var loginUrl: String? = null,                // 登录地址
+    override var loginUrl: String? = null,                // 登录地址
     var loginUi: List<RowUi>? = null,             //登录UI
     var loginCheckJs: String? = null,               //登录检测js
     var bookSourceComment: String? = null,          // 注释
