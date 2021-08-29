57a23999d639e4b3366012b421df0da5c72747f7
Jason Yao <h11129@gmail.com>
2021-08-28 22:20:19 -0400

    adjust through upgrade testing

--- app/src/main/java/io/legado/app/help/BookSourceAnalyzer.kt
+++ app/src/main/java/io/legado/app/help/BookSourceAnalyzer.kt
@@ -161,7 +161,7 @@ object BookSourceAnalyzer {
         var loginCheckJs: String? = null,               //登录检测js
         var bookSourceComment: String? = "",             //书源注释
         var lastUpdateTime: Long = 0,                   // 最后更新时间，用于排序
-        var respondTime: Long? = 180000L,         // 响应时间，用于排序
+        var respondTime: Long = 180000L,                // 响应时间，用于排序
         var weight: Int = 0,                            // 智能排序的权重
         var exploreUrl: String? = null,                 // 发现url
         var ruleExplore: Any? = null,           // 发现规则
