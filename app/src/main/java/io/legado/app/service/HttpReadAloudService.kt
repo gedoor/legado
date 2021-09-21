348d80446be198fdea167b8dda34b892d46c9f12
gedoor <gekunfei@live.com>
2021-09-21 23:43:03 +0800

    优化

--- app/src/main/java/io/legado/app/service/HttpReadAloudService.kt
+++ app/src/main/java/io/legado/app/service/HttpReadAloudService.kt
@@ -265,6 +265,7 @@ class HttpReadAloudService : BaseReadAloudService(),
         errorNo++
         if (errorNo >= 3) {
             toastOnUi("朗读连续3次错误, 最后一次错误代码($what, $extra)")
+            ReadAloud.pause(this)
         } else {
             playNext()
         }
