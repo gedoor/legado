1a6d59389d0296ae4031d7457057d2b2dc29c190
gedoor <gekunfei@live.com>
2021-10-06 23:57:37 +0800

    优化

--- app/src/main/java/io/legado/app/ui/widget/dialog/CodeDialog.kt
+++ app/src/main/java/io/legado/app/ui/widget/dialog/CodeDialog.kt
@@ -9,14 +9,15 @@ import io.legado.app.lib.theme.primaryColor
 import io.legado.app.ui.widget.code.addJsPattern
 import io.legado.app.ui.widget.code.addJsonPattern
 import io.legado.app.ui.widget.code.addLegadoPattern
+import io.legado.app.utils.disableEdit
 import io.legado.app.utils.setLayout
 import io.legado.app.utils.viewbindingdelegate.viewBinding
 
 class CodeDialog() : BaseDialogFragment(R.layout.dialog_code_view) {
 
-    constructor(code: String) : this() {
+    constructor(code: String, disableEdit: Boolean = true) : this() {
         arguments = Bundle().apply {
-
+            putBoolean("disableEdit", disableEdit)
             putString("code", code)
         }
     }
@@ -30,12 +31,37 @@ class CodeDialog() : BaseDialogFragment(R.layout.dialog_code_view) {
 
     override fun onFragmentCreated(view: View, savedInstanceState: Bundle?) {
         binding.toolBar.setBackgroundColor(primaryColor)
+        if (arguments?.getBoolean("disableEdit") == true) {
+            binding.codeView.disableEdit()
+        } else {
+            initMenu()
+        }
         binding.codeView.addLegadoPattern()
         binding.codeView.addJsonPattern()
         binding.codeView.addJsPattern()
         arguments?.getString("code")?.let {
             binding.codeView.setText(it)
         }
+    }
+
+    private fun initMenu() {
+        binding.toolBar.inflateMenu(R.menu.code_edit)
+        binding.toolBar.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.menu_save -> binding.codeView.text?.toString()?.let { code ->
+                    (parentFragment as? Callback)?.saveCode(code)
+                        ?: (activity as? Callback)?.saveCode(code)
+                }
+            }
+            return@setOnMenuItemClickListener true
+        }
+    }
+
+
+    interface Callback {
+
+        fun saveCode(code: String)
+
     }
 
 }
