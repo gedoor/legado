ddcaba27f360991ebb857881268c2a52559acecc
gedoor <gekunfei@live.com>
2021-10-06 09:09:57 +0800

    优化web

--- app/src/main/java/io/legado/app/ui/widget/code/CodeViewExtensions.kt
+++ app/src/main/java/io/legado/app/ui/widget/code/CodeViewExtensions.kt
@@ -5,29 +5,31 @@ package io.legado.app.ui.widget.code
 import android.content.Context
 import android.widget.ArrayAdapter
 import io.legado.app.R
-import io.legado.app.lib.theme.accentColor
-import io.legado.app.lib.theme.secondaryTextColor
 import splitties.init.appCtx
+import splitties.resources.color
 import java.util.regex.Pattern
 
 val legadoPattern: Pattern = Pattern.compile("\\|\\||&&|%%|@js:|@Json:|@css:|@@|@XPath:")
 val jsonPattern: Pattern = Pattern.compile("\"[A-Za-z0-9]*?\"\\:|\"|\\{|\\}|\\[|\\]")
 val wrapPattern: Pattern = Pattern.compile("\\\\n")
-val jsPattern: Pattern = Pattern.compile("var|=")
+val operationPattern: Pattern =
+    Pattern.compile(":|==|>|<|!=|>=|<=|->|=|>|<|%|-|-=|%=|\\+|\\-|\\-=|\\+=|\\^|\\&|\\|::|\\?|\\*")
+val jsPattern: Pattern = Pattern.compile("var")
 
 fun CodeView.addLegadoPattern() {
-    addSyntaxPattern(legadoPattern, appCtx.accentColor)
+    addSyntaxPattern(legadoPattern, appCtx.color(R.color.md_orange_900))
 }
 
 fun CodeView.addJsonPattern() {
-    addSyntaxPattern(jsonPattern, appCtx.accentColor)
+    addSyntaxPattern(jsonPattern, appCtx.color(R.color.md_blue_800))
 }
 
 fun CodeView.addJsPattern() {
-    addSyntaxPattern(wrapPattern, appCtx.secondaryTextColor)
-    addSyntaxPattern(jsPattern, appCtx.accentColor)
+    addSyntaxPattern(wrapPattern, appCtx.color(R.color.md_blue_grey_500))
+    addSyntaxPattern(operationPattern, appCtx.color(R.color.md_orange_900))
+    addSyntaxPattern(jsPattern, appCtx.color(R.color.md_light_blue_600))
 }
 
 fun Context.arrayAdapter(keywords: Array<String>): ArrayAdapter<String> {
-    return ArrayAdapter(this, R.layout.item_text, R.id.text_view, keywords)
+    return ArrayAdapter(this, R.layout.item_1line_text_and_del, R.id.text_view, keywords)
 }
