99d54eb2625ecf3699400a80bdf76d2b9f333ea8
gedoor <gekunfei@live.com>
2021-10-26 11:16:45 +0800

    优化

--- app/src/main/java/io/legado/app/model/analyzeRule/AnalyzeRule.kt
+++ app/src/main/java/io/legado/app/model/analyzeRule/AnalyzeRule.kt
@@ -262,9 +262,10 @@ class AnalyzeRule(
     /**
      * 获取Element
      */
-    fun getElement(ruleStr: String): Any? {
+    fun getElement(ruleStr: String, mContent: Any? = null): Any? {
         if (TextUtils.isEmpty(ruleStr)) return null
         var result: Any? = null
+        val content = mContent ?: this.content
         val ruleList = splitSourceRule(ruleStr, true)
         content?.let { o ->
             if (ruleList.isNotEmpty()) result = o
@@ -295,8 +296,9 @@ class AnalyzeRule(
      * 获取列表
      */
     @Suppress("UNCHECKED_CAST")
-    fun getElements(ruleStr: String): List<Any> {
+    fun getElements(ruleStr: String, mContent: Any? = null): List<Any> {
         var result: Any? = null
+        val content = mContent ?: this.content
         val ruleList = splitSourceRule(ruleStr, true)
         content?.let { o ->
             if (ruleList.isNotEmpty()) result = o
