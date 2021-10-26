23c57d61ae86c42ec387a04be2a0d55368694774
gedoor <gekunfei@live.com>
2021-10-26 15:26:44 +0800

    优化

--- app/src/main/java/io/legado/app/ui/widget/dialog/CodeDialog.kt
+++ app/src/main/java/io/legado/app/ui/widget/dialog/CodeDialog.kt
@@ -2,6 +2,7 @@ package io.legado.app.ui.widget.dialog
 
 import android.os.Bundle
 import android.view.View
+import android.view.ViewGroup
 import io.legado.app.R
 import io.legado.app.base.BaseDialogFragment
 import io.legado.app.databinding.DialogCodeViewBinding
@@ -28,7 +29,7 @@ class CodeDialog() : BaseDialogFragment(R.layout.dialog_code_view) {
 
     override fun onStart() {
         super.onStart()
-        setLayout(0.9f, 0.9f)
+        setLayout(ViewGroup.LayoutParams.MATCH_PARENT, 0.9f)
     }
 
     override fun onFragmentCreated(view: View, savedInstanceState: Bundle?) {
