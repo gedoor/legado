55f133d27c5760baa92953296f5d469a415d44fc
gedoor <gekunfei@live.com>
2021-11-15 23:15:02 +0800

    优化

--- app/src/main/java/io/legado/app/ui/book/local/rule/TxtTocRuleActivity.kt
+++ app/src/main/java/io/legado/app/ui/book/local/rule/TxtTocRuleActivity.kt
@@ -10,10 +10,16 @@ class TxtTocRuleActivity : VMBaseActivity<ActivityTxtTocRuleBinding, TxtTocRuleV
 
     override val viewModel: TxtTocRuleViewModel by viewModels()
     override val binding: ActivityTxtTocRuleBinding by viewBinding(ActivityTxtTocRuleBinding::inflate)
+    private val adapter: TxtTocRuleAdapter by lazy {
+        TxtTocRuleAdapter(this)
+    }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         TODO("Not yet implemented")
     }
 
+    private fun initView() {
+
+    }
 
 }
