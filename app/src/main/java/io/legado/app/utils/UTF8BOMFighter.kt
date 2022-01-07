100218060efe8a293c5f176d996ef94e42071154
gedoor <gekunfei@live.com>
2022-01-07 10:46:48 +0800

    优化

--- app/src/main/java/io/legado/app/utils/UTF8BOMFighter.kt
+++ app/src/main/java/io/legado/app/utils/UTF8BOMFighter.kt
@@ -18,14 +18,21 @@ object UTF8BOMFighter {
 
     fun removeUTF8BOM(bytes: ByteArray): ByteArray {
         val containsBOM = (bytes.size > 3
-                && bytes[0] == UTF8_BOM_BYTES[0]
-                && bytes[1] == UTF8_BOM_BYTES[1]
-                && bytes[2] == UTF8_BOM_BYTES[2])
+            && bytes[0] == UTF8_BOM_BYTES[0]
+            && bytes[1] == UTF8_BOM_BYTES[1]
+            && bytes[2] == UTF8_BOM_BYTES[2])
         if (containsBOM) {
             val copy = ByteArray(bytes.size - 3)
             System.arraycopy(bytes, 3, copy, 0, bytes.size - 3)
             return copy
         }
         return bytes
+    }
+
+    fun hasBom(bytes: ByteArray): Boolean {
+        return (bytes.size > 3
+            && bytes[0] == UTF8_BOM_BYTES[0]
+            && bytes[1] == UTF8_BOM_BYTES[1]
+            && bytes[2] == UTF8_BOM_BYTES[2])
     }
 }
