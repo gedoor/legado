f03ed16cbe70acdad755e45bbee8e6d4ecc89e12
gedoor <gekunfei@live.com>
2022-01-23 13:32:30 +0800

    优化

--- app/src/main/java/io/legado/app/ui/book/search/SearchActivity.kt
+++ app/src/main/java/io/legado/app/ui/book/search/SearchActivity.kt
@@ -30,7 +30,6 @@ import io.legado.app.utils.*
 import io.legado.app.utils.viewbindingdelegate.viewBinding
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.Job
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
