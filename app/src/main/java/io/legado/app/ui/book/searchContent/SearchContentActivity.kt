21c40b08d608eedcaf495c27b418d86aea1301c1
Xwite <1797350009@qq.com>
2022-01-30 08:21:14 +0800

    全文搜索：搜索完成后关闭搜索显示，未搜索到内容提示用户

--- app/src/main/java/io/legado/app/ui/book/searchContent/SearchContentActivity.kt
+++ app/src/main/java/io/legado/app/ui/book/searchContent/SearchContentActivity.kt
@@ -148,16 +148,20 @@ class SearchContentActivity :
                             searchResults = viewModel.searchChapter(query, bookChapter)
                         }
                     }
+                    binding.tvCurrentSearchInfo.text = this@SearchContentActivity.getString(R.string.search_content_size) +": ${viewModel.searchResultCounts}"
                     if (searchResults.isNotEmpty()) {
                         viewModel.searchResultList.addAll(searchResults)
                         binding.refreshProgressBar.isAutoLoading = false
-                        binding.tvCurrentSearchInfo.text = this@SearchContentActivity.getString(R.string.search_content_size) +": ${viewModel.searchResultCounts}"
                         adapter.addItems(searchResults)
                         searchResults = listOf()
                     }
                 }
+                binding.refreshProgressBar.isAutoLoading = false
+                if (viewModel.searchResultCounts == 0) {
+                    val noSearchResult = SearchResult(resultText = getString(R.string.search_content_empty))
+                    adapter.addItem(noSearchResult)
+                }
             }
-            binding.refreshProgressBar.isAutoLoading = false
         }
     }
 
