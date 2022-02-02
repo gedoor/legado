c3c8649010341df40e73ab1019d218313f275594
Xwite <1797350009@qq.com>
2022-02-02 22:13:52 +0800

    fix:同时校验搜索发现时，搜索正常发现为空时 校验停止的bug

--- app/src/main/java/io/legado/app/service/CheckSourceService.kt
+++ app/src/main/java/io/legado/app/service/CheckSourceService.kt
@@ -144,15 +144,18 @@ class CheckSourceService : BaseService() {
                     }
                 }
                 if (url.isNullOrBlank()) {
-                    if (source.hasGroup("搜索失效")) {
-                        throw NoStackTraceException("搜索内容为空并且没有发现")
-                    } else {
-                        throw NoStackTraceException("没有发现")
+                    when {
+                        !CheckSource.checkSearch -> throw NoStackTraceException("没有发现")
+                        source.hasGroup("搜索失效") -> throw NoStackTraceException("搜索内容为空并且没有发现")
                     }
-                }
-                books = WebBook.exploreBookAwait(this, source, url)
-                if (books.isEmpty()) {
-                    throw NoStackTraceException("发现书籍为空")
+                } else {
+                    books = WebBook.exploreBookAwait(this, source, url)
+                    if (books.isEmpty()) {
+                        when {
+                            !CheckSource.checkSearch -> throw NoStackTraceException("发现书籍为空")
+                            source.hasGroup("搜索失效") -> throw NoStackTraceException("搜索内容和发现书籍为空")
+                        }
+                    }
                 }
             }
             //校验详情
