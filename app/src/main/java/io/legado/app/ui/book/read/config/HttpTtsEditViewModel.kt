21dc1f63af77886b4753732ce311bffdf9ff9787
Xwite <1797350009@qq.com>
2022-02-09 19:42:22 +0800

    移除重复的upReadAloudClass()

--- app/src/main/java/io/legado/app/ui/book/read/config/HttpTtsEditViewModel.kt
+++ app/src/main/java/io/legado/app/ui/book/read/config/HttpTtsEditViewModel.kt
@@ -34,7 +34,6 @@ class HttpTtsEditViewModel(app: Application) : BaseViewModel(app) {
         id = httpTTS.id
         execute {
             appDb.httpTTSDao.insert(httpTTS)
-            ReadAloud.upReadAloudClass()
         }.onSuccess {
             success?.invoke()
         }
