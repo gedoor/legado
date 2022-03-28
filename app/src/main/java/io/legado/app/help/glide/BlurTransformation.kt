7d144920d5f75fff15514e24e098b503c806c60d
kunfei <gekunfei@live.com>
2022-03-28 21:18:29 +0800

    优化

--- app/src/main/java/io/legado/app/help/glide/BlurTransformation.kt
+++ app/src/main/java/io/legado/app/help/glide/BlurTransformation.kt
@@ -8,8 +8,6 @@ import com.bumptech.glide.load.engine.bitmap_recycle.BitmapPool
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import io.legado.app.utils.stackBlur
 import java.security.MessageDigest
-import kotlin.math.min
-import kotlin.math.roundToInt
 
 
 /**
@@ -28,11 +26,7 @@ class BlurTransformation(
         outHeight: Int
     ): Bitmap {
         val transform = super.transform(pool, toTransform, outWidth, outHeight)
-        //图片缩小1/2
-        val width = (min(outWidth, transform.width) / 5f).roundToInt()
-        val height = (min(outHeight, transform.height) / 5f).roundToInt()
-        val blurredBitmap = Bitmap.createScaledBitmap(transform, width, height, false)
-        return blurredBitmap.stackBlur(radius)
+        return transform.stackBlur(radius)
     }
 
     override fun updateDiskCacheKey(messageDigest: MessageDigest) {
