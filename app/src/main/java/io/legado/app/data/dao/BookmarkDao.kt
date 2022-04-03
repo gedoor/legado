10a703c189c394b7aef470eb9c572fb37fcd717f
kunfei <gekunfei@live.com>
2022-04-03 09:05:43 +0800

    添加所有书签界面

--- app/src/main/java/io/legado/app/data/dao/BookmarkDao.kt
+++ app/src/main/java/io/legado/app/data/dao/BookmarkDao.kt
@@ -7,7 +7,11 @@ import io.legado.app.data.entities.Bookmark
 @Dao
 interface BookmarkDao {
 
-    @get:Query("select * from bookmarks order by bookName, bookAuthor, chapterIndex, chapterPos")
+    @get:Query(
+        """
+        select * from bookmarks order by bookName collate localized, bookAuthor collate localized, chapterIndex, chapterPos
+    """
+    )
     val all: List<Bookmark>
 
     @Query(
