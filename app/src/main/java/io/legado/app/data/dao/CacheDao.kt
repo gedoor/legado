6abf5002eb3df105e96da0020fcb602bdb04a9ba
kunfei <gekunfei@live.com>
2022-05-05 08:51:03 +0800

    优化

--- app/src/main/java/io/legado/app/data/dao/CacheDao.kt
+++ app/src/main/java/io/legado/app/data/dao/CacheDao.kt
@@ -9,6 +9,9 @@ import io.legado.app.data.entities.Cache
 @Dao
 interface CacheDao {
 
+    @Query("select * from caches where `key` = :key")
+    fun get(key: String): Cache?
+
     @Query("select value from caches where `key` = :key and (deadline = 0 or deadline > :now)")
     fun get(key: String, now: Long): String?
 
