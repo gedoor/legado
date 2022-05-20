8de034935009651aea7b8350587e8f514e139e76
kunfei <gekunfei@live.com>
2022-05-20 17:33:24 +0800

    优化

--- app/src/main/java/io/legado/app/ui/about/DonateFragment.kt
+++ app/src/main/java/io/legado/app/ui/about/DonateFragment.kt
@@ -12,10 +12,14 @@ import io.legado.app.utils.*
 
 class DonateFragment : PreferenceFragmentCompat() {
 
-    private val zfbHbRwmUrl = "https://gitee.com/gekunfei/Donate/raw/master/zfbhbrwm.png"
-    private val zfbSkRwmUrl = "https://gitee.com/gekunfei/Donate/raw/master/zfbskrwm.jpg"
-    private val wxZsRwmUrl = "https://gitee.com/gekunfei/Donate/raw/master/wxskrwm.jpg"
-    private val qqSkRwmUrl = "https://gitee.com/gekunfei/Donate/raw/master/qqskrwm.jpg"
+    private val zfbHbRwmUrl =
+        "https://www.legado.top/assets/images/zfbhbrwm-6dfbcd1d680cfd831b93490a91052656.png"
+    private val zfbSkRwmUrl =
+        "https://www.legado.top/assets/images/zfbskrwm-66379bdee8214093872696e413f6dda9.jpg"
+    private val wxZsRwmUrl =
+        "https://www.legado.top/assets/images/wxskrwm-d8e6963d6ae122a3c2e818f3c4bc09cf.jpg"
+    private val qqSkRwmUrl =
+        "https://www.legado.top/assets/images/qqskrwm-2c10b25f67f4354eec5ab5bd6080285f.jpg"
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         addPreferencesFromResource(R.xml.donate)
