1fbcc92dafe621e497becc6d6bc6ae2d2521c90d
kunfei <gekunfei@live.com>
2022-05-27 20:26:22 +0800

    优化

--- app/src/main/java/io/legado/app/data/entities/rule/BookInfoRule.kt
+++ app/src/main/java/io/legado/app/data/entities/rule/BookInfoRule.kt
@@ -17,5 +17,6 @@ data class BookInfoRule(
     var tocUrl: String? = null,
     var wordCount: String? = null,
     var canReName: String? = null,
-    var downloadUrls: String? = null
+    var downloadUrls: String? = null,
+    var reGetTocUrlOnRefresh: String? = null
 ) : Parcelable
