85800523f77b181e39fd144de44ba0c1d2d4099e
821938089 <821938089@qq.com>
2022-07-02 17:51:06 +0800

    fix

--- app/src/main/java/io/legado/app/model/CacheBook.kt
+++ app/src/main/java/io/legado/app/model/CacheBook.kt
@@ -216,7 +216,7 @@ object CacheBook {
             }
             val chapter = appDb.bookChapterDao.getChapter(book.bookUrl, chapterIndex) ?: let {
                 waitDownloadSet.remove(chapterIndex)
-                return false
+                return@download false
             }
             if (BookHelp.hasContent(book, chapter)) {
                 waitDownloadSet.remove(chapterIndex)
