17a8b4f1ff6706e61557e09d06db0ba6f6cce3d2
821938089 <821938089@qq.com>
2022-07-05 11:10:08 +0800

    优化

--- app/src/main/java/io/legado/app/lib/webdav/WebDav.kt
+++ app/src/main/java/io/legado/app/lib/webdav/WebDav.kt
@@ -172,14 +172,11 @@ open class WebDav(val path: String, val authorization: Authorization) {
      */
     suspend fun exists(): Boolean {
         return kotlin.runCatching {
-            val requestPropsStr = DIR.replace("%s", "")
             return okHttpClient.newCallResponse {
                 url(url)
                 addHeader(authorization.name, authorization.data)
-                addHeader("Depth", "0")
-                val requestBody = requestPropsStr.toRequestBody("application/xml".toMediaType())
-                method("PROPFIND", requestBody)
-            }.code == 207
+                head()
+            }.code == 200
         }.getOrDefault(false)
     }
 
