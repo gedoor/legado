da5599a6ff77ef53507a05b173ae74a843c0a76d
kunfei <gekunfei@live.com>
2022-09-13 23:03:27 +0800

    优化

--- app/src/main/java/io/legado/app/ui/login/SourceLoginDialog.kt
+++ app/src/main/java/io/legado/app/ui/login/SourceLoginDialog.kt
@@ -33,7 +33,7 @@ class SourceLoginDialog : BaseDialogFragment(R.layout.dialog_login) {
 
     override fun onStart() {
         super.onStart()
-        setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+        setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
     }
 
     override fun onFragmentCreated(view: View, savedInstanceState: Bundle?) {
