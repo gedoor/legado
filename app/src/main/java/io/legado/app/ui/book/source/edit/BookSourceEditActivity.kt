25f80a6e9117fedefd97c30ae9c0443e62a0fd9e
Xwite <1797350009@qq.com>
2022-09-15 14:38:35 +0800

    feat: add imageDecode Rule

--- app/src/main/java/io/legado/app/ui/book/source/edit/BookSourceEditActivity.kt
+++ app/src/main/java/io/legado/app/ui/book/source/edit/BookSourceEditActivity.kt
@@ -297,6 +297,7 @@ class BookSourceEditActivity :
             add(EditEntity("sourceRegex", cr?.sourceRegex, R.string.rule_source_regex))
             add(EditEntity("replaceRegex", cr?.replaceRegex, R.string.rule_replace_regex))
             add(EditEntity("imageStyle", cr?.imageStyle, R.string.rule_image_style))
+            add(EditEntity("imageDecode", cr?.imageDecode, R.string.rule_image_decode))
             add(EditEntity("payAction", cr?.payAction, R.string.rule_pay_action))
         }
         // 段评
@@ -451,6 +452,7 @@ class BookSourceEditActivity :
                 "sourceRegex" -> contentRule.sourceRegex = it.value
                 "replaceRegex" -> contentRule.replaceRegex = it.value
                 "imageStyle" -> contentRule.imageStyle = it.value
+                "imageDecode" -> contentRule.imageDecode = it.value
                 "payAction" -> contentRule.payAction = it.value
             }
         }
