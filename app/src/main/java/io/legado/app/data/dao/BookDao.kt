5f05bb0b631c71649d14cf8297cb7f6892de70f2
kunfei <gekunfei@live.com>
2022-09-23 22:24:23 +0800

    优化

--- app/src/main/java/io/legado/app/data/dao/BookDao.kt
+++ app/src/main/java/io/legado/app/data/dao/BookDao.kt
@@ -38,7 +38,7 @@ interface BookDao {
 
     @Query(
         """
-        select * from books where type != ${BookType.audio} and origin != '${BookType.local}' 
+        select * from books where type != ${BookType.audio} and origin = '${BookType.local}' 
         and ((SELECT sum(groupId) FROM book_groups where groupId > 0) & `group`) = 0
         """
     )
