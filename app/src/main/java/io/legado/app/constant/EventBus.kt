6433d17a685db9364a351594cc165cc54d204cb8
Xwite <1797350009@qq.com>
2022-10-01 09:11:22 +0800

    chore: rename EventBus.BOOK_URL_CHANGED

--- app/src/main/java/io/legado/app/constant/EventBus.kt
+++ app/src/main/java/io/legado/app/constant/EventBus.kt
@@ -28,6 +28,6 @@ object EventBus {
     const val TIP_COLOR = "tipColor"
     const val SOURCE_CHANGED = "sourceChanged"
     const val SEARCH_RESULT = "searchResult"
-    const val BOOK_URL_CHANGED = "bookUrlChanged"
+    const val FILE_SOURCE_DOWNLOAD_DONE = "fileSourceDownloadDone"
     const val updateReadActionBar = "updateReadActionBar"
 }
