f0ff76be383a17cc84a148f73ae7a8bad8594dc3
Horis <821938089@qq.com>
2022-10-02 12:45:54 +0800

    优化

--- app/src/main/java/io/legado/app/ui/about/DonateFragment.kt
+++ app/src/main/java/io/legado/app/ui/about/DonateFragment.kt
@@ -13,13 +13,13 @@ import io.legado.app.utils.*
 class DonateFragment : PreferenceFragmentCompat() {
 
     private val zfbHbRwmUrl =
-        "https://www.legado.top/assets/images/zfbhbrwm-6dfbcd1d680cfd831b93490a91052656.png"
+        "https://gedoor.github.io/assets/images/zfbhbrwm-6dfbcd1d680cfd831b93490a91052656.png"
     private val zfbSkRwmUrl =
-        "https://www.legado.top/assets/images/zfbskrwm-66379bdee8214093872696e413f6dda9.jpg"
+        "https://gedoor.github.io/assets/images/zfbskrwm-66379bdee8214093872696e413f6dda9.jpg"
     private val wxZsRwmUrl =
-        "https://www.legado.top/assets/images/wxskrwm-d8e6963d6ae122a3c2e818f3c4bc09cf.jpg"
+        "https://gedoor.github.io/assets/images/wxskrwm-d8e6963d6ae122a3c2e818f3c4bc09cf.jpg"
     private val qqSkRwmUrl =
-        "https://www.legado.top/assets/images/qqskrwm-2c10b25f67f4354eec5ab5bd6080285f.jpg"
+        "https://gedoor.github.io/assets/images/qqskrwm-2c10b25f67f4354eec5ab5bd6080285f.jpg"
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         addPreferencesFromResource(R.xml.donate)
