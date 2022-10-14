72b6d34096e5fe3ba0232d1984c56ab05460d102
kunfei <gekunfei@live.com>
2022-10-14 08:40:55 +0800

    优化

--- app/src/main/java/io/legado/app/ui/book/search/SearchScopeDialog.kt
+++ app/src/main/java/io/legado/app/ui/book/search/SearchScopeDialog.kt
@@ -48,7 +48,7 @@ class SearchScopeDialog : BaseDialogFragment(R.layout.dialog_search_scope) {
     }
 
     private fun initMenu() {
-        binding.toolBar.inflateMenu(R.menu.change_source)
+        binding.toolBar.inflateMenu(R.menu.book_search_scope)
         binding.toolBar.menu.applyTint(requireContext())
     }
 
