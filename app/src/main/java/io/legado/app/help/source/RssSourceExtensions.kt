e9711815fe55d22385dc40a1914be4f0448da674
kunfei <gekunfei@live.com>
2022-10-16 18:27:35 +0800

    优化

--- app/src/main/java/io/legado/app/help/source/RssSourceExtensions.kt
+++ app/src/main/java/io/legado/app/help/source/RssSourceExtensions.kt
@@ -37,7 +37,7 @@ suspend fun RssSource.sortUrls(): List<Pair<String, String>> {
                     val name = sort.substringBefore("::")
                     val url = sort.substringAfter("::", "")
                     if (url.isNotEmpty()) {
-                        add(Pair(name, NetworkUtils.getAbsoluteURL(sortUrl, url)))
+                        add(Pair(name, NetworkUtils.getAbsoluteURL(sourceUrl, url)))
                     }
                 }
                 if (isEmpty()) {
