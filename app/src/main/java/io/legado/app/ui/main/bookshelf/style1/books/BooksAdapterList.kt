9f906c5cf9c842648409d423a132c78fb91ba5cf
Horis <821938089@qq.com>
2022-10-29 19:25:35 +0800

    优化

--- app/src/main/java/io/legado/app/ui/main/bookshelf/style1/books/BooksAdapterList.kt
+++ app/src/main/java/io/legado/app/ui/main/bookshelf/style1/books/BooksAdapterList.kt
@@ -65,7 +65,7 @@ class BooksAdapterList(context: Context, private val callBack: CallBack) :
     }
 
     private fun upLastUpdateTime(binding: ItemBookshelfListBinding, item: Book) {
-        if (!item.isLocal && item.canUpdate && AppConfig.showLastUpdateTime) {
+        if (AppConfig.showLastUpdateTime && !item.isLocal && item.canUpdate) {
             binding.tvLastUpdateTime.text = item.latestChapterTime.toTimeAgo()
         } else {
             binding.tvLastUpdateTime.text = ""
