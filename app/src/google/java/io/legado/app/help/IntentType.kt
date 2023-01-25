ece0c656120571cb06f5f5eca3457c916abdc76b
kunfei <gekunfei@live.com>
2023-01-25 17:54:19 +0800

    优化

--- app/src/google/java/io/legado/app/help/IntentType.kt
+++ app/src/google/java/io/legado/app/help/IntentType.kt
@@ -1,24 +1,22 @@
-package io.legado.app.utils
+package io.legado.app.help
 
 import android.net.Uri
+import androidx.annotation.Keep
 import java.io.File
 
+@Keep
 object IntentType {
 
-    @JvmStatic
     fun from(uri: Uri): String? {
         return from(uri.toString())
     }
 
-    @JvmStatic
     fun from(file: File): String? {
         return from(file.absolutePath)
     }
 
-    @JvmStatic
     fun from(path: String?): String? {
         return when (path?.substringAfterLast(".")?.lowercase()) {
-            "apk" -> "application/vnd.android.package-archive"
             "m4a", "mp3", "mid", "xmf", "ogg", "wav" -> "video/*"
             "3gp", "mp4" -> "audio/*"
             "jpg", "gif", "png", "jpeg", "bmp" -> "image/*"
