2ddacfd06bb9e3886abc92cc10bf4ea88d017a25
Horis <821938089@qq.com>
2023-01-25 11:20:29 +0800

    优化

--- app/src/main/java/io/legado/app/help/book/BookHelp.kt
+++ app/src/main/java/io/legado/app/help/book/BookHelp.kt
@@ -118,15 +118,13 @@ object BookHelp {
         content: String
     ) = coroutineScope {
         val awaitList = arrayListOf<Deferred<Unit>>()
-        content.split("\n").forEach {
-            val matcher = AppPattern.imgPattern.matcher(it)
-            while (matcher.find()) {
-                matcher.group(1)?.let { src ->
-                    val mSrc = NetworkUtils.getAbsoluteURL(bookChapter.url, src)
-                    awaitList.add(async {
-                        saveImage(bookSource, book, mSrc)
-                    })
-                }
+        val matcher = AppPattern.imgPattern.matcher(content)
+        while (matcher.find()) {
+            matcher.group(1)?.let { src ->
+                val mSrc = NetworkUtils.getAbsoluteURL(bookChapter.url, src)
+                awaitList.add(async {
+                    saveImage(bookSource, book, mSrc)
+                })
             }
         }
         awaitList.forEach {
