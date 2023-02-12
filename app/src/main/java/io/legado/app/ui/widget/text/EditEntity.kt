834a499c62c4abfd09b65548bd614263a216c4b5
kunfei <gekunfei@live.com>
2023-02-12 21:46:55 +0800

    优化

--- app/src/main/java/io/legado/app/ui/widget/text/EditEntity.kt
+++ app/src/main/java/io/legado/app/ui/widget/text/EditEntity.kt
@@ -2,8 +2,16 @@ package io.legado.app.ui.widget.text
 
 import splitties.init.appCtx
 
-data class EditEntity(var key: String, var value: String?, var hint: String) {
+data class EditEntity(
+    var key: String,
+    var value: String?,
+    var hint: String
+) {
 
-    constructor(key: String, value: String?, hint: Int) : this(key, value, appCtx.getString(hint))
+    constructor(
+        key: String,
+        value: String?,
+        hint: Int
+    ) : this(key, value, appCtx.getString(hint))
 
 }
