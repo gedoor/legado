5a53432081e96043a8c15be2ee00a033928592d4
kunfei <gekunfei@live.com>
2023-03-02 23:05:24 +0800

    优化

--- app/src/main/java/io/legado/app/ui/book/import/remote/RemoteBookActivity.kt
+++ app/src/main/java/io/legado/app/ui/book/import/remote/RemoteBookActivity.kt
@@ -92,6 +92,7 @@ class RemoteBookActivity : BaseImportBookActivity<ActivityImportBookBinding, Rem
     override fun onCompatOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.menu_refresh -> upPath()
+            R.id.menu_server_config -> {}
             R.id.menu_log -> showDialogFragment<AppLogDialog>()
             R.id.menu_help -> showHelp("webDavBookHelp")
             R.id.menu_sort_name -> {
