b900c4821ef0b783665ed92f1b398da2f41c3dbc
kunfei <gekunfei@live.com>
2023-03-15 11:39:13 +0800

    优化

--- app/src/main/java/io/legado/app/help/crypto/Sign.kt
+++ app/src/main/java/io/legado/app/help/crypto/Sign.kt
@@ -1,8 +1,9 @@
 package io.legado.app.help.crypto
 
-import cn.hutool.crypto.asymmetric.Sign as HutoolSign
 import cn.hutool.crypto.KeyUtil
+import cn.hutool.crypto.asymmetric.Sign as HutoolSign
 
+@Suppress("unused")
 class Sign(algorithm: String): HutoolSign(algorithm) {
 
     fun setPrivateKey(key: ByteArray): Sign {
@@ -11,6 +12,7 @@ class Sign(algorithm: String): HutoolSign(algorithm) {
         )
         return this
     }
+
     fun setPrivateKey(key: String): Sign = setPrivateKey(key.encodeToByteArray())
 
     fun setPublicKey(key: ByteArray): Sign {
@@ -19,6 +21,7 @@ class Sign(algorithm: String): HutoolSign(algorithm) {
         )
         return this
     }
+
     fun setPublicKey(key: String): Sign = setPublicKey(key.encodeToByteArray())
 
 }
