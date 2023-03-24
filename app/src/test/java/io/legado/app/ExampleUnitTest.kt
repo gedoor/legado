218e56bf98022ebaffbf1ee5fb7a33e4287967a2
kunfei <gekunfei@live.com>
2023-03-24 23:50:01 +0800

    优化

--- app/src/test/java/io/legado/app/ExampleUnitTest.kt
+++ app/src/test/java/io/legado/app/ExampleUnitTest.kt
@@ -1,7 +1,10 @@
 package io.legado.app
 
+import com.script.SimpleBindings
+import com.script.rhino.RhinoScriptEngine
 import org.junit.Assert.assertEquals
 import org.junit.Test
+
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,4 +16,18 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+
+    @Test
+    fun jsTest() {
+        val scriptEngine = RhinoScriptEngine()
+        val map = hashMapOf("id" to "3242532321")
+        map["id"] = "12314123"
+        val bindings = SimpleBindings()
+        bindings["result"] = map
+        val js = "$=result;id=$.id;id"
+        val result = scriptEngine.eval(js, bindings)?.toString()
+        assertEquals("12314123", result)
+    }
+
+
 }
