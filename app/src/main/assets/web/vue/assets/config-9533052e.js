01b3c8ec2e6cf772c19d0e8b3569813a09bd45ab
Xwite <Xwite@users.noreply.github.com>
2023-04-08 00:45:37 +0000

    Bump web vv3.23.040808

--- app/src/main/assets/web/vue/assets/config-9533052e.js
+++
@@ -1,1 +0,0 @@
-import{u as n,A as r}from"./index-68a3d477.js";import"./vendor-c4d61938.js";const i=n();i.setMiniInterface(window.innerWidth<750);window.onresize=()=>{i.setMiniInterface(window.innerWidth<750)};r.getReadConfig().then(a=>{var e=a.data.data;if(e){const t=n();let o=JSON.parse(e),s=t.config;o=Object.assign(s,o),t.setConfig(o)}});
