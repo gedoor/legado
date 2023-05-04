6121a98a78a6236dcc12abd1b2f433a79e54c873
Xwite <1797350009@qq.com>
2023-05-04 13:30:07 +0800

    fix(web): 源修改后列表多选异常 添加编辑按钮
    close #3051

--- modules/web/src/components/SourceItem.vue
+++ modules/web/src/components/SourceItem.vue
@@ -2,28 +2,43 @@
   <el-checkbox
     size="large"
     border
-    :label="source"
-    :class="{ error: errorPushSources.includes(source) }"
-    @change="handleSourceClick(source)"
+    :label="sourceUrl"
+    :class="{
+      error: errorPushSources.includes(source),
+      edit: sourceUrl == currentSourceUrl,
+    }"
   >
     {{ source.bookSourceName || source.sourceName }}
+    <el-button text :icon="Edit" @click="handleSourceClick(source)" />
   </el-checkbox>
 </template>
 
 <script setup>
-defineProps(["source"]);
+import { Edit } from "@element-plus/icons-vue";
+
+const props = defineProps(["source"]);
+const sourceUrl = props.source.bookSourceUrl || props.source.sourceUrl;
 const store = useSourceStore();
-const { errorPushSources } = storeToRefs(store);
+const { errorPushSources, currentSourceUrl } = storeToRefs(store);
 const handleSourceClick = (source) => {
   store.changeCurrentSource(source);
 };
 </script>
 <style lang="scss" scoped>
+:deep(.el-checkbox__label) {
+  flex: 1;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
 .error {
   border-color: var(--el-color-error) !important;
   color: var(--el-color-error) !important;
   --el-checkbox-checked-text-color: var(--el-color-error);
   --el-checkbox-checked-bg-color: var(--el-color-error);
   --el-checkbox-checked-input-border-color: var(--el-color-error);
+}
+.edit {
+  border-color: var(--el-color-dark) !important;
 }
 </style>
