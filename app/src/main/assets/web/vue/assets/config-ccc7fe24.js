575647aeb5ad60d9976ec12407a7dd4cedc6614b
StanHustler <cf260469080@sina.com>
2023-09-23 22:23:43 +0800

    build

---
+++ app/src/main/assets/web/vue/assets/config-ccc7fe24.js
@@ -0,0 +1,1 @@
+import{A as f,u as i}from"./index-6758c83f.js";import"./vendor-260e64da.js";f.getReadConfig().then(e=>{var t=e.data.data;if(t){const a=i();let o=JSON.parse(t),s=a.config;o=Object.assign(s,o),a.setConfig(o)}});
