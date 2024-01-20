f55cade258a79b2b8cef4d3f6f2c1d0223dbc95b
821938089 <821938089@users.noreply.github.com>
2024-01-20 02:14:45 +0000

    Bump web v3.24.012010

---
+++ app/src/main/assets/web/vue/assets/config-dddfc601.js
@@ -0,0 +1,1 @@
+import{A as f,u as i}from"./index-5b3b591d.js";import"./vendor-117f3db1.js";f.getReadConfig().then(e=>{var t=e.data.data;if(t){const a=i();let o=JSON.parse(t),s=a.config;o=Object.assign(s,o),a.setConfig(o)}});
