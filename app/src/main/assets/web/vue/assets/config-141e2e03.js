805582efb44bb9260cf7e0382e4380de0a2bf0f1
Antecer <Antecer@users.noreply.github.com>
2024-02-20 05:42:36 +0000

    Bump web v3.24.022013

--- app/src/main/assets/web/vue/assets/config-141e2e03.js
+++
@@ -1,1 +0,0 @@
-import{A as f,u as i}from"./index-e9653d8d.js";import"./vendor-4a0d1fab.js";f.getReadConfig().then(e=>{var t=e.data.data;if(t){const a=i();let o=JSON.parse(t),s=a.config;o=Object.assign(s,o),a.setConfig(o)}});
