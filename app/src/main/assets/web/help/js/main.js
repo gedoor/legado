bc5fc7cc033cc04f9d3fdab9d63536d0e7319f5a
Antecer <antecer@outlook.com>
2024-04-06 03:01:44 +0800

    修复md图片显示

--- app/src/main/assets/web/help/js/main.js
+++ app/src/main/assets/web/help/js/main.js
@@ -1,31 +1,34 @@
 require.config({
     baseUrl: 'js',
     paths: {
-        markdownit: 'markdown-it.min',
+        marked: 'marked.min',
+        markedHighlight: 'marked-highlight.umd',
         highlight: 'highlight.min',
     },
     shim: {
+        marked: {
+            exports: 'marked',
+        },
+        markedHighlight: {
+            exports: 'markedHighlight',
+        },
         highlight: {
             exports: 'hljs',
         },
     },
 });
 
-require(['markdownit', 'highlight'], (mdit, hljs) => {
-    const md = mdit({
-        html: false, // 在源代码中启用 HTML 标签
-        xhtmlOut: false, // 使用“/”关闭单个标签 (<br />)。 这仅用于完全兼容 CommonMark。
-        breaks: false, // 将段落中的 '\n' 转换为 <br>
-        langPrefix: 'language-', // 代码块的 CSS 属性前缀。用于代码高亮插件。
-        linkify: false, // 自动将类似 URL 的文本转换为链接
-        typographer: false, // 启用一些语言中立的替换+引号美化
-        quotes: '“”‘’',
-        highlight: function (code, lang) {
-            let language = hljs.getLanguage(lang)?.name ?? 'plaintext';
-            if (language.includes('HTML')) language = 'xml';
-            return hljs.highlight(code, {language}).value;
-        },
-    });
+require(['marked', 'markedHighlight', 'highlight'], (marked, mdhl, hljs) => {
+    marked.use(
+        mdhl.markedHighlight({
+            langPrefix: 'theme-vs2015-min hljs language-',
+            highlight(code, lang) {
+                const language = hljs.getLanguage(lang) ? lang : 'plaintext';
+                const result = hljs.highlight(code, {language});
+                return result.value;
+            },
+        })
+    );
 
     const path = '/help/md/';
     const file = location.hash.slice(1).trim();
@@ -33,6 +36,6 @@ require(['markdownit', 'highlight'], (mdit, hljs) => {
     fetch(`${path}${file}.md`)
         .then((response) => response.text())
         .then((md_text) => {
-            document.getElementById('mdviewer').innerHTML = md.render(md_text);
+            document.getElementById('mdviewer').innerHTML = marked.parse(md_text);
         });
 });
