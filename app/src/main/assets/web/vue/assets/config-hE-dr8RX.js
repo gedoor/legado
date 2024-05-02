d7ac10d05c38d6156ce088ab2fb0f345277a2338
Antecer <antecer@outlook.com>
2024-05-02 13:06:25 +0800

    回滚依赖库

---
+++ app/src/main/assets/web/vue/assets/config-hE-dr8RX.js
@@ -0,0 +1,1 @@
+import{A as f,u as n}from"./index-eiEvjBAi.js";import"./vendor-CNdhk-0s.js";f.getReadConfig().then(e=>{var t=e.data.data;if(t){const a=n();let o=JSON.parse(t),i=a.config;o=Object.assign(i,o),a.setConfig(o)}});
