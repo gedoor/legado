3685431b407252c683a3582a572d559d56694a4f
Horis <8674809+821938089@users.noreply.github.com>
2024-08-16 20:25:20 +0800

    优化

--- app/src/main/java/io/legado/app/App.kt
+++ app/src/main/java/io/legado/app/App.kt
@@ -161,7 +161,7 @@ class App : Application() {
         val readAloudChannel = NotificationChannel(
             channelIdReadAloud,
             getString(R.string.read_aloud),
-            NotificationManager.IMPORTANCE_DEFAULT
+            NotificationManager.IMPORTANCE_HIGH
         ).apply {
             enableLights(false)
             enableVibration(false)
