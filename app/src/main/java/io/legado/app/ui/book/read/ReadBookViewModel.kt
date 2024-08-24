caa3a7c6ed825aa4608d321e89c27862a48c3f1c
Horis <8674809+821938089@users.noreply.github.com>
2024-08-24 20:31:57 +0800

    优化

--- app/src/main/java/io/legado/app/ui/book/read/ReadBookViewModel.kt
+++ app/src/main/java/io/legado/app/ui/book/read/ReadBookViewModel.kt
@@ -265,7 +265,7 @@ class ReadBookViewModel(application: Application) : BaseViewModel(application) {
             ReadBook.upMsg(null)
             ReadBook.loadContent(resetPageOffset = true)
         }.onError {
-            context.toastOnUi("换源失败\n${it.localizedMessage}")
+            AppLog.put("换源失败\n$it", it, true)
             ReadBook.upMsg(null)
         }.onFinally {
             postEvent(EventBus.SOURCE_CHANGED, book.bookUrl)
