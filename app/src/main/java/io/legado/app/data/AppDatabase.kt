6a22728f874227286b7ec1823e60dd225a13f24f
niuhb <niuakl@163.com>
2024-09-26 10:17:47 +0800

    优化数据库版本控制

--- app/src/main/java/io/legado/app/data/AppDatabase.kt
+++ app/src/main/java/io/legado/app/data/AppDatabase.kt
@@ -63,7 +63,7 @@ val appDb by lazy {
 }
 
 @Database(
-    version = 73,
+    version = 72,
     exportSchema = true,
     entities = [Book::class, BookGroup::class, BookSource::class, BookChapter::class,
         ReplaceRule::class, SearchBook::class, SearchKeyword::class, Cookie::class,
@@ -100,7 +100,6 @@ val appDb by lazy {
         AutoMigration(from = 69, to = 70),
         AutoMigration(from = 70, to = 71),
         AutoMigration(from = 71, to = 72),
-        AutoMigration(from = 72, to = 73),
     ]
 )
 abstract class AppDatabase : RoomDatabase() {
