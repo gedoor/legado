c88849b075760b0b1750cd2370152a937a9e15bf
Xwite <Xwite@users.noreply.github.com>
2024-09-29 12:04:57 +0000

    Bump web v3.24.092920

--- app/src/main/assets/web/vue/assets/config-BsTATf_v.js
+++
@@ -1,1 +0,0 @@
-import{A as f}from"./index-CyZ_tLuT.js";import{u as r}from"./bookStore-O1g6rLjj.js";import"./vendor-UVL3l2rb.js";f.getReadConfig().then(e=>{var t=e.data.data;if(t){const a=r();let o=JSON.parse(t),i=a.config;o=Object.assign(i,o),a.setConfig(o)}});
