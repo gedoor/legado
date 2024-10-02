7911bbaf5816ae216e170e8280445c2f7dab7953
Xwite <82232510+Xwite@users.noreply.github.com>
2024-10-02 22:12:50 +0800

    [skip ci] fix(module/web/pages/source): import ElementPlus darkmode css var

--- modules/web/src/pages/source/main.js
+++ modules/web/src/pages/source/main.js
@@ -2,5 +2,6 @@ import { createApp } from "vue";
 import App from "@/App.vue";
 import sourceRouter from "@/router/sourceRouter";
 import store from "@/store";
+import "element-plus/theme-chalk/dark/css-vars.css";
 
 createApp(App).use(store).use(sourceRouter).mount("#app");
