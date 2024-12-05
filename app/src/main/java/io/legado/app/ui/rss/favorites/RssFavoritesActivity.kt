47d5d7064a9bc4ad96ea40a93558e67f4009b011
niuhb <niuakl@163.com>
2024-12-05 17:24:39 +0800

    rss收藏页面增加分组查看和跳转

--- app/src/main/java/io/legado/app/ui/rss/favorites/RssFavoritesActivity.kt
+++ app/src/main/java/io/legado/app/ui/rss/favorites/RssFavoritesActivity.kt
@@ -3,9 +3,13 @@
 package io.legado.app.ui.rss.favorites
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.SubMenu
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentStatePagerAdapter
 import androidx.lifecycle.lifecycleScope
+import io.legado.app.R
 import io.legado.app.base.BaseActivity
 import io.legado.app.constant.AppLog
 import io.legado.app.data.appDb
@@ -28,6 +32,7 @@ class RssFavoritesActivity : BaseActivity<ActivityRssFavoritesBinding>() {
     override val binding by viewBinding(ActivityRssFavoritesBinding::inflate)
     private val adapter by lazy { TabFragmentPageAdapter() }
     private var groupList = mutableListOf<String>()
+    private var groupsMenu: SubMenu? = null
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         initView()
@@ -38,6 +43,28 @@ class RssFavoritesActivity : BaseActivity<ActivityRssFavoritesBinding>() {
         binding.viewPager.adapter = adapter
         binding.tabLayout.setupWithViewPager(binding.viewPager)
         binding.tabLayout.setSelectedTabIndicatorColor(accentColor)
+    }
+
+    override fun onCompatCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_explore, menu)
+        groupsMenu = menu.findItem(R.id.menu_group)?.subMenu
+        upGroupsMenu()
+        return super.onCompatCreateOptionsMenu(menu)
+    }
+
+    private fun upGroupsMenu() = groupsMenu?.let { subMenu ->
+        subMenu.removeGroup(R.id.menu_group_text)
+        groupList.forEachIndexed { index, it ->
+            subMenu.add(R.id.menu_group_text, Menu.NONE, index, it)
+        }
+    }
+
+    override fun onCompatOptionsItemSelected(item: MenuItem): Boolean {
+        super.onCompatOptionsItemSelected(item)
+        if (item.groupId == R.id.menu_group_text) {
+            binding.viewPager.setCurrentItem(item.order)
+        }
+        return super.onCompatOptionsItemSelected(item)
     }
 
     private fun upFragments() {
@@ -51,6 +78,9 @@ class RssFavoritesActivity : BaseActivity<ActivityRssFavoritesBinding>() {
                     binding.tabLayout.gone()
                 } else {
                     binding.tabLayout.visible()
+                }
+                if (groupsMenu != null){
+                    upGroupsMenu()
                 }
                 adapter.notifyDataSetChanged()
             }
