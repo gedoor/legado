d11fa95e015f7f06f0e5f44e770ee35f6313c070
ag2s20150909 <19373730+ag2s20150909@users.noreply.github.com>
2025-01-17 23:07:49 +0800

    Update UmdUtils.java

--- modules/book/src/main/java/me/ag2s/umdlib/tool/UmdUtils.java
+++ modules/book/src/main/java/me/ag2s/umdlib/tool/UmdUtils.java
@@ -3,14 +3,13 @@ package me.ag2s.umdlib.tool;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
-import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Random;
-import java.util.zip.InflaterInputStream;
+import java.util.zip.Inflater;
 
 
 public class UmdUtils {
