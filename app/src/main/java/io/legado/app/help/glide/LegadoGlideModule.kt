485c2277d695c2de563800bd4774d87e54842f02
DESKTOP-K7T1R2S\lhj <1079947827@qq.com>
2025-01-22 20:42:50 +0800

    ...

--- app/src/main/java/io/legado/app/help/glide/LegadoGlideModule.kt
+++ app/src/main/java/io/legado/app/help/glide/LegadoGlideModule.kt
@@ -6,11 +6,14 @@ import com.bumptech.glide.GlideBuilder
 import com.bumptech.glide.Registry
 import com.bumptech.glide.annotation.GlideModule
 import com.bumptech.glide.load.DecodeFormat
+import com.bumptech.glide.load.engine.bitmap_recycle.LruBitmapPool
+import com.bumptech.glide.load.engine.cache.InternalCacheDiskCacheFactory
 import com.bumptech.glide.load.engine.cache.LruResourceCache
 import com.bumptech.glide.load.model.GlideUrl
 import com.bumptech.glide.module.AppGlideModule
 import com.bumptech.glide.request.RequestOptions
 import io.legado.app.help.glide.progress.ProgressManager.glideProgressInterceptor
+import io.legado.app.utils.externalCache
 import java.io.InputStream
 
 
@@ -30,6 +33,13 @@ class LegadoGlideModule : AppGlideModule() {
     override fun applyOptions(context: Context, builder: GlideBuilder) {
         super.applyOptions(context, builder)
         builder.setMemoryCache(LruResourceCache(1024 * 1024 * 500))
+            .setBitmapPool(LruBitmapPool(1024 * 1024 * 200))
+            .setDiskCache(
+                InternalCacheDiskCacheFactory(
+                    context,
+                    1024 * 1024 * 1000
+                )
+            )
             .setDefaultRequestOptions {
                 RequestOptions().format(DecodeFormat.PREFER_RGB_565)
                     .encodeQuality(90)
