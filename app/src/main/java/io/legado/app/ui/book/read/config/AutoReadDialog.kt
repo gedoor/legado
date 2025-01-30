74a2ebe30b5660d3a024f930ec1592b164968b06
niuhb <niuakl@163.com>
2025-01-30 19:36:42 +0800

    自动翻页速度下限调整为1秒

--- app/src/main/java/io/legado/app/ui/book/read/config/AutoReadDialog.kt
+++ app/src/main/java/io/legado/app/ui/book/read/config/AutoReadDialog.kt
@@ -22,6 +22,7 @@ import io.legado.app.ui.book.read.ReadBookActivity
 import io.legado.app.ui.widget.seekbar.SeekBarChangeListener
 import io.legado.app.utils.ColorUtils
 import io.legado.app.utils.viewbindingdelegate.viewBinding
+import java.util.Locale
 
 
 class AutoReadDialog : BaseDialogFragment(R.layout.dialog_auto_read) {
@@ -74,21 +75,21 @@ class AutoReadDialog : BaseDialogFragment(R.layout.dialog_auto_read) {
     }
 
     private fun initData() {
-        val speed = if (ReadBookConfig.autoReadSpeed < 2) 2 else ReadBookConfig.autoReadSpeed
-        binding.tvReadSpeed.text = String.format("%ds", speed)
+        val speed = if (ReadBookConfig.autoReadSpeed < 1) 1 else ReadBookConfig.autoReadSpeed
+        binding.tvReadSpeed.text = String.format(Locale.ROOT, "%ds", speed)
         binding.seekAutoRead.progress = speed
     }
 
     private fun initOnChange() {
         binding.seekAutoRead.setOnSeekBarChangeListener(object : SeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
-                val speed = if (progress < 2) 2 else progress
-                binding.tvReadSpeed.text = String.format("%ds", speed)
+                val speed = if (progress < 1) 1 else progress
+                binding.tvReadSpeed.text = String.format(Locale.ROOT,"%ds", speed)
             }
 
             override fun onStopTrackingTouch(seekBar: SeekBar) {
                 ReadBookConfig.autoReadSpeed =
-                    if (binding.seekAutoRead.progress < 2) 2 else binding.seekAutoRead.progress
+                    if (binding.seekAutoRead.progress < 1) 1 else binding.seekAutoRead.progress
                 upTtsSpeechRate()
             }
         })
