7548b0b101c0b5789cbcd66cc5660dcbeb1baafc
WIN-20240701WVD\Administrator <1079947827@qq.com>
2025-02-20 15:52:20 +0800

    ...

---
+++ app/src/main/java/io/legado/app/ui/book/manga/config/MangaFooterConfig.kt
@@ -0,0 +1,4 @@
+package io.legado.app.ui.book.manga.config
+
+class MangaFooterEvent {
+}
---
+++ app/src/main/java/io/legado/app/ui/book/manga/config/MangaFooterSettingDialog.kt
@@ -0,0 +1,6 @@
+package io.legado.app.ui.book.manga.config
+
+import io.legado.app.base.BaseDialogFragment
+
+class FooterSettingDialog:BaseDialogFragment() {
+}
