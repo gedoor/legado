405d0546d3deafac9e016ff53e9a14222492a34c
Horis <8674809+821938089@users.noreply.github.com>
2025-04-06 20:55:30 +0800

    优化

--- app/src/main/java/io/legado/app/App.kt
+++ app/src/main/java/io/legado/app/App.kt
@@ -57,7 +57,6 @@ class App : Application() {
         CrashHandler(this)
         LogUtils.init(this)
         LogUtils.d("App", "onCreate")
-        LogUtils.logDeviceInfo()
         if (isDebuggable) {
             ThreadUtils.setThreadAssertsDisabledForTesting(true)
         }
@@ -76,6 +75,7 @@ class App : Application() {
         DefaultData.upVersion()
         AppFreezeMonitor.init(this)
         Coroutine.async {
+            LogUtils.logDeviceInfo()
             URL.setURLStreamHandlerFactory(ObsoleteUrlFactory(okHttpClient))
             launch { installGmsTlsProvider(appCtx) }
             RhinoScriptEngine
