4f09bbcf51c7268d36d30f2ab35d81abb54dc75a
Horis <8674809+821938089@users.noreply.github.com>
2025-10-01 21:28:44 +0800

    优化

--- app/src/main/java/io/legado/app/ui/book/read/ReadBookActivity.kt
+++ app/src/main/java/io/legado/app/ui/book/read/ReadBookActivity.kt
@@ -1329,8 +1329,13 @@ class ReadBookActivity : BaseReadBookActivity(),
                 ReadAloud.upReadAloudClass()
                 val scrollPageAnim = ReadBook.pageAnim() == 3
                 if (scrollPageAnim) {
-                    val startPos = binding.readView.getCurPagePosition()
-                    ReadBook.readAloud(startPos = startPos)
+                    val line = binding.readView.getCurVisibleFirstLine()
+                    if (line != null) {
+                        ReadBook.durChapterPos = line.chapterPosition
+                        ReadBook.readAloud(startPos = line.pagePosition)
+                    } else {
+                        ReadBook.readAloud()
+                    }
                 } else {
                     ReadBook.readAloud()
                 }
@@ -1340,8 +1345,13 @@ class ReadBookActivity : BaseReadBookActivity(),
                 val scrollPageAnim = ReadBook.pageAnim() == 3
                 if (scrollPageAnim && pageChanged) {
                     pageChanged = false
-                    val startPos = binding.readView.getCurPagePosition()
-                    ReadBook.readAloud(startPos = startPos)
+                    val line = binding.readView.getCurVisibleFirstLine()
+                    if (line != null) {
+                        ReadBook.durChapterPos = line.chapterPosition
+                        ReadBook.readAloud(startPos = line.pagePosition)
+                    } else {
+                        ReadBook.readAloud()
+                    }
                 } else {
                     ReadAloud.resume(this)
                 }
@@ -1644,6 +1654,7 @@ class ReadBookActivity : BaseReadBookActivity(),
             lifecycleScope.launch(IO) {
                 if (BaseReadAloudService.isPlay()) {
                     ReadBook.curTextChapter?.let { textChapter ->
+                        ReadBook.durChapterPos = chapterStart
                         val pageIndex = ReadBook.durPageIndex
                         val aloudSpanStart = chapterStart - textChapter.getReadLength(pageIndex)
                         textChapter.getPage(pageIndex)
