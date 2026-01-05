d174aa93198594f7d30d15e9625224898ec1e5b3
Horis <8674809+821938089@users.noreply.github.com>
2026-01-05 10:45:08 +0800

    优化 #5637

--- app/src/main/java/io/legado/app/help/CacheManager.kt
+++ app/src/main/java/io/legado/app/help/CacheManager.kt
@@ -86,7 +86,6 @@ object CacheManager {
         }
         val cache = appDb.cacheDao.get(key)
         if (cache != null && (cache.deadline == 0L || cache.deadline > System.currentTimeMillis())) {
-            putMemory(key, cache.value ?: "")
             return cache.value
         }
         return null
